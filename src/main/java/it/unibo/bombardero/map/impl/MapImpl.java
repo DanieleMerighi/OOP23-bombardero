4c2e64c3fbe4b007297dc05289a045d8ef9ebe60
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 14:18:16 +0100

    Started implementation of MapImpl class

---
+++ src/main/java/it/unibo/bombardero/map/impl/MapImpl.java
@@ -0,0 +1,67 @@
+package it.unibo.bombardero.map.impl;
+
+import it.unibo.bombardero.map.api.Map;
+
+public class MapImpl implements Map {
+
+    @Override
+    public void addBomb() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addBomb'");
+    }
+
+    @Override
+    public void addEnemy() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addEnemy'");
+    }
+
+    @Override
+    public void addPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addPlayer'");
+    }
+
+    @Override
+    public void removeEnemy() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeEnemy'");
+    }
+
+    @Override
+    public boolean isPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPlayer'");
+    }
+
+    @Override
+    public boolean isEnemy() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEnemy'");
+    }
+
+    @Override
+    public boolean isBomb() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isBomb'");
+    }
+
+    @Override
+    public boolean isBreakableWall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isBreakableWall'");
+    }
+
+    @Override
+    public boolean isUnbreakableWall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isUnbreakableWall'");
+    }
+
+    @Override
+    public boolean isExplosion() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isExplosion'");
+    }
+    
+}
