324d19ffc8df87e876cd052f22b255c877294b2d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 21:39:05 +0100

    Added fields

--- src/main/java/it/unibo/bombardero/view/BombarderoWindow.java
+++ src/main/java/it/unibo/bombardero/view/BombarderoWindow.java
@@ -1,8 +1,13 @@
 package it.unibo.bombardero.view;
 
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 
 public class BombarderoWindow extends JFrame {
+
+    private final ResourceGetter resourceGetter = new ResourceGetter();
+    private final BufferedImage staticMap = resourceGetter.loadImage("map");
     
     public BombarderoWindow() {
         this.pack();
