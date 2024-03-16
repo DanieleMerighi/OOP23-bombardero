4fa3822c3587f7dbbcb3f36360365176246d8595
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 22:10:07 +0100

    Added fields

--- src/main/java/it/unibo/bombardero/view/BombarderoWindow.java
+++ src/main/java/it/unibo/bombardero/view/BombarderoWindow.java
@@ -1,13 +1,22 @@
 package it.unibo.bombardero.view;
 
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class BombarderoWindow extends JFrame {
 
     private final ResourceGetter resourceGetter = new ResourceGetter();
+    private final ResizingEngine resizingEngine = new ResizingEngine();
     private final BufferedImage staticMap = resourceGetter.loadImage("map");
+    private JPanel deck = new JPanel(new CardLayout());
+    private CardLayout layout = (CardLayout)deck.getLayout();
     
     public BombarderoWindow() {
         this.pack();
