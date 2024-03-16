d52b81b8c8d7c9f4789ff5624931765b32ae4e8a
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 21:36:08 +0100

    ResourceGetter helper class implementation

---
+++ src/main/java/it/unibo/bombardero/view/ResourceGetter.java
@@ -0,0 +1,27 @@
+package it.unibo.bombardero.view;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/** 
+ * A class that fetches the files in the resources/ directory given the name
+ * @author Federico Bagattoni   
+ */
+
+public class ResourceGetter {
+
+    private final static String RESOURCE_PATH = "it/unibo/bombardero/";
+
+    public BufferedImage loadImage(final String name) {
+        try {
+            return ImageIO.read(ClassLoader.getSystemResource(RESOURCE_PATH + name));
+        }
+        catch(IOException e) {
+            System.out.println("There was an exception loading " + name + " resource.\n");
+        }
+        return null;
+    }
+}
+
