efcf197d7eaac96e8e67c85be38fc809c11181b4
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 14:18:05 +0100

    Added map interface

---
+++ src/main/java/it/unibo/bombardero/map/api/Map.java
@@ -0,0 +1,28 @@
+package it.unibo.bombardero.map.api;
+
+/**
+ * The main class for managing all the aspects of the game arena
+ * @author Federico Bagattoni
+ */
+public interface Map {
+
+    void addBomb();
+
+    void addEnemy();
+
+    void addPlayer();
+
+    void removeEnemy();
+
+    boolean isPlayer();
+
+    boolean isEnemy();
+
+    boolean isBomb();
+
+    boolean isBreakableWall();
+
+    boolean isUnbreakableWall();
+
+    boolean isExplosion();
+}
