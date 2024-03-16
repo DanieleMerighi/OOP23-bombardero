620b7fc5690e17016e1009c67942b42044b9aabd
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 15:21:03 +0100

    Added the game's main window

---
+++ src/main/java/it/unibo/bombardero/view/BombarderoWindow.java
@@ -0,0 +1,11 @@
+package it.unibo.bombardero.view;
+
+import javax.swing.JFrame;
+
+public class BombarderoWindow extends JFrame {
+    
+    public BombarderoWindow() {
+        this.pack();
+        this.setVisible(true);
+    }
+}
