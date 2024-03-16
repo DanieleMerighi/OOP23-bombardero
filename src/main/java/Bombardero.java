72976ac4a79be0788f39dc90e6e9d5e9f4780abb
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-16 15:20:51 +0100

    Main class now creating a window

--- src/main/java/Bombardero.java
+++ src/main/java/Bombardero.java
@@ -1,6 +1,8 @@
+import it.unibo.bombardero.view.BombarderoWindow;
+
 public class Bombardero {
     
     public static void main(String[] args) {
-        
+        new BombarderoWindow();
     }
 }
