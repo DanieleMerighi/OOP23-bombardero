bb4270f20c2c0cad406ddba96e00f0c51b907bda
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-17 15:57:23 +0100

    Created class heading

---
+++ src/main/java/it/unibo/bombardero/map/impl/MapManager.java
@@ -0,0 +1,9 @@
+package it.unibo.bombardero.map.impl;
+
+/**
+ * A class that manages the dynamic aspects of the Arena, particularly the gradual narrowing
+ * of the external bounds once the time is up and the generation of the walls
+ */
+public class MapManager {
+    
+}
