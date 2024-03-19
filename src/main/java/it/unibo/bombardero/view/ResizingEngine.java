13c8382c36e72950072ff95d44ff25319ed8245d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-19 11:39:56 +0100

    Commented method

--- src/main/java/it/unibo/bombardero/view/ResizingEngine.java
+++ src/main/java/it/unibo/bombardero/view/ResizingEngine.java
@@ -17,6 +17,13 @@ public class ResizingEngine {
 
     public 
 
+    /**
+     * Computes the size that the window has to be in order to display correctly the panel, 
+     * plus the size of the frame's insets
+     * @param panelSize the size of the panel that the @frame has to contain
+     * @param frame the frame, from which to get the insets, assuming the insets have been already generated
+     * @return the total frame size accounting the panel and the insets in every direction
+     */
     public Dimension computeTotalWindowSize(Dimension panelSize, JFrame frame) {
         return new Dimension(
             frame.getInsets().left + frame.getInsets().right + (int)panelSize.getWidth(), 
