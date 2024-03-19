017b1e6e9f209a4a21dbfc90c893bd73d2f7dfbd
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-19 11:27:25 +0100

    Added TO-DO

--- src/main/java/it/unibo/bombardero/view/ResizingEngine.java
+++ src/main/java/it/unibo/bombardero/view/ResizingEngine.java
@@ -1,5 +1,12 @@
 package it.unibo.bombardero.view;
 
+/*
+ * Qui deve essere fatto il coso per calcolare la dimensione della finestra inset inclusi
+ * e per calcolare la scale a cui sarà scalato tutto 
+ * Dopo una certa dimensione della finestra, l'immagine rimarrà costante e ci aggiungeremo qualcosa di sfondo 
+ * giusto per non lasciarla vuota 
+ */
+
 public class ResizingEngine {
     
 }
