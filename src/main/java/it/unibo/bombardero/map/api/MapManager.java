6dea50aeb4d3faf2966ec95b044df05c8ab6d136
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-24 17:08:44 +0100

    Written map manager interface and comment

--- src/main/java/it/unibo/bombardero/map/api/MapManager.java
+++ src/main/java/it/unibo/bombardero/map/api/MapManager.java
@@ -1,5 +1,18 @@
 package it.unibo.bombardero.map.api;
 
+/** 
+ * The inteface describing the dynamic aspects of the Game Map: placement of breakable
+ * and unbreakable walls and the logic behind the map's collapse when the timer ends
+ *  
+ */
 public interface MapManager {
+
+    void generateBreakableWalls();
+
+	void placeUnbreakableWalls();
+
+    void placeNextWall();
+
     
+
 }
