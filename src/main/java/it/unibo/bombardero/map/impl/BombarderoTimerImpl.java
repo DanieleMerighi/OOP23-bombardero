e2395d4e74461f559ebcc35e4699ffb2c41fe9a3
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-25 21:26:24 +0100

    Blank methods

--- src/main/java/it/unibo/bombardero/map/impl/BombarderoTimerImpl.java
+++ src/main/java/it/unibo/bombardero/map/impl/BombarderoTimerImpl.java
@@ -1,5 +1,25 @@
 package it.unibo.bombardero.map.impl;
 
-public class BombarderoTimerImpl {
+import it.unibo.bombardero.map.api.BombarderoTimer;
+
+public class BombarderoTimerImpl implements BombarderoTimer {
+
+    @Override
+    public void startTimer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startTimer'");
+    }
+
+    @Override
+    public void updateTimer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateTimer'");
+    }
+
+    @Override
+    public void getTimeLeft() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTimeLeft'");
+    }
     
 }
