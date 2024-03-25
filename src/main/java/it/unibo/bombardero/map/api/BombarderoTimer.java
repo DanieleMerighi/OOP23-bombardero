568d07566ead82a6626e347972539c36a1c318a0
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-25 21:07:24 +0100

    Added BombarderoTimer interface

---
+++ src/main/java/it/unibo/bombardero/map/api/BombarderoTimer.java
@@ -0,0 +1,11 @@
+package it.unibo.bombardero.map.api;
+
+/** 
+ * This interface models the in-game countdown timer that triggers the arena's collapse
+ * and is displayed on the top portion of the screen
+ * @author Federico Bagattoni
+ */
+
+public interface BombarderoTimer {
+    
+}
