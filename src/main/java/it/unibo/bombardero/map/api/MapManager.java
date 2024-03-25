e9585bfc2c96b98ef408954835e610e9946c8f55
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-25 21:13:31 +0100

    Added author

--- src/main/java/it/unibo/bombardero/map/api/MapManager.java
+++ src/main/java/it/unibo/bombardero/map/api/MapManager.java
@@ -3,7 +3,7 @@ package it.unibo.bombardero.map.api;
 /** 
  * The inteface describing the dynamic aspects of the Game Map: placement of breakable
  * and unbreakable walls and the logic behind the map's collapse when the timer ends
- *  
+ *  @author Federico Bagattoni
  */
 public interface MapManager {
 
