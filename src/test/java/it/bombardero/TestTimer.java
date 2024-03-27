a81e53387f8a2d282b49972cf2b30c1094d96475
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-03-27 21:09:21 +0100

    Implementation of test class for BombarderoTimer

--- src/test/java/it/bombardero/TestTimer.java
+++ src/test/java/it/bombardero/TestTimer.java
@@ -1,9 +1,39 @@
 package it.bombardero;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.bombardero.map.api.BombarderoTimer;
+import it.unibo.bombardero.map.impl.BombarderoTimerImpl;
+
 /** 
- * This class tests if the BombarderTimerImpl class
- * works properly
+ * @author Federico Bagattoni
  */
 public class TestTimer {
+
+    private BombarderoTimer timer;
+    private long startTimeForTest;
+
+    @BeforeEach void init() {
+        this.timer = new BombarderoTimerImpl();
+        this.startTimeForTest = System.currentTimeMillis();
+        timer.startTimer();
+    }
     
+    @Test void testGetTimeLeftAsLong() {
+        this.timer.updateTimer();
+        assertEquals(this.timer.getTimeLeft(), System.currentTimeMillis() - this.startTimeForTest);
+    }
+
+    /**
+     * This method prints out the formatted result for visual verification:
+     * the string has to be something on the line of "4:36"
+     */
+    @Test void testGetTimeLeftAsString() {
+        this.timer.updateTimer();
+        System.out.println(this.timer.getFormattedTimeLeft());
+    }
+
 }
