4f21e4ee6693ee14ec942ea2a2e59696e06a4cc5
Daniele Merighi <daniele.merighi2@studio.unibo.it>
2024-03-29 12:41:20 +0100

    Added other value for the map

--- src/main/java/it/unibo/bombardero/utils/Utils.java
+++ src/main/java/it/unibo/bombardero/utils/Utils.java
@@ -16,6 +16,9 @@ public class Utils {
     public static final int UNBREAKABLE_WALL = 2;
     public static final int WALL = 3;
     public static final int PLAYER = 4;
+    public static final int ENEMY = 5;
+    public static final int BOMB = 6;
+    public static final int EXPLOSION = 7;
 
     // Valori per il timer
     public final static long GAME_TIME = 120000l;
