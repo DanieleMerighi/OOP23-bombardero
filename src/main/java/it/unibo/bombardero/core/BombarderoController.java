0d8e759a4a8dc4b56ac2f4079e0be84ea51ebb30
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-03-29 17:48:55 +0100

    Initial commit, created class BombarderoController

---
+++ src/main/java/it/unibo/bombardero/core/BombarderoController.java
@@ -0,0 +1,13 @@
+package it.unibo.bombardero.core;
+
+import java.util.List;
+
+import it.unibo.bombardero.map.api.GameMap;
+
+public class BombarderoController {
+
+    private GameMap map;
+    private List<Character> characters;
+
+    
+}
