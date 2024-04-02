a504d9a2e16e93cd6ec21496e3489ddb7caa78a8
Daniele Merighi <daniele.merighi2@studio.unibo.it>
2024-04-02 17:08:14 +0200

    Bug fixes

--- src/main/java/it/unibo/bombardero/character/AI/GraphBuilder.java
+++ src/main/java/it/unibo/bombardero/character/AI/GraphBuilder.java
@@ -10,34 +10,13 @@ import org.jgrapht.graph.SimpleGraph;
 
 import it.unibo.bombardero.map.api.Pair;
 import it.unibo.bombardero.utils.Utils;
+import it.unibo.bombardero.character.Direction;
+
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
 
 public class GraphBuilder {
-
-    enum Direction {
-        UP(-1, 0),
-        DOWN(1, 0),
-        LEFT(0, -1),
-        RIGHT(0, 1);
-
-        private final int dx;
-        private final int dy;
-
-        Direction(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-
-        public int getDx() {
-            return dx;
-        }
-
-        public int getDy() {
-            return dy;
-        }
-    }
 
     public static Graph<Pair,DefaultEdge> buildFromMap(int[][] map) {
         int numRows = map.length;
@@ -49,7 +28,7 @@ public class GraphBuilder {
         IntStream.range(0, numRows)
                 .forEach(row ->
                         IntStream.range(0, numCols)
-                            .filter(col -> map[row][col] == Utils.GRASS || map[row][col] == Utils.PLAYER || map[row][col] == Utils.ENEMY)
+                                .filter(col -> !isCellBlocked(map, row, col))
                                 .forEach(col -> graph.addVertex(new Pair(row, col))));
 
         // Add edges (connections between adjacent cells) to the graph
@@ -57,16 +36,18 @@ public class GraphBuilder {
                 .forEach(row ->
                         IntStream.range(0, numCols)
                                 .filter(col -> map[row][col] == Utils.GRASS)
-                                .forEach(col -> connectWithNeighbors(graph, row, col, numRows, numCols)));
+                                .forEach(col -> connectWithNeighbors(graph, map, row, col, numRows, numCols)));
 
         return graph;
     }
 
-    private static void connectWithNeighbors(Graph<Pair, DefaultEdge> graph, int row, int col, int numRows, int numCols) {
+    private static void connectWithNeighbors(Graph<Pair, DefaultEdge> graph, int[][] map, int row, int col, int numRows, int numCols) {
         EnumSet.allOf(Direction.class).forEach(direction -> {
             int newRow = row + direction.getDx();
             int newCol = col + direction.getDy();
-            if (isValidCell(newRow, newCol, numRows, numCols) && graph.containsVertex(new Pair(newRow, newCol))) {
+            if (isValidCell(newRow, newCol, numRows, numCols) 
+            && graph.containsVertex(new Pair(newRow, newCol))
+            && !isCellBlocked(map, newRow, newCol)) {
                 graph.addEdge(new Pair(row, col), new Pair(newRow, newCol));
             }
         });
@@ -76,10 +57,14 @@ public class GraphBuilder {
         return row >= 0 && row < numRows && col >= 0 && col < numCols;
     }
 
+    private static boolean isCellBlocked(int[][] map, int row, int col) {
+        return map[row][col] == Utils.UNBREAKABLE_WALL || map[row][col] == Utils.EXPLOSION;
+    }
+
     public static Optional<List<Pair>> findShortestPath(Graph<Pair, DefaultEdge> graph, Pair source, Pair target) {
         DijkstraShortestPath<Pair, DefaultEdge> dijkstra = new DijkstraShortestPath<>(graph);
         GraphPath<Pair, DefaultEdge> path = dijkstra.getPath(source, target);
-        return path == null ? Optional.empty() : Optional.of(path.getVertexList());
+        return path == null ? Optional.empty() : Optional.of(path.getVertexList().subList(1, path.getVertexList().size()));
     }
     
 }
