8752841ed5ca92b413d6c4590540d39b75a7af1b
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-04-03 10:47:26 +0200

    changed to interface GameManager

---
+++ src/main/java/it/unibo/bombardero/core/api/GameManager.java
@@ -0,0 +1,21 @@
+package it.unibo.bombardero.core.api;
+
+import java.util.List;
+
+import it.unibo.bombardero.map.api.GameMap;
+
+public interface GameManager {
+
+    void startGame();
+
+    void updateGame();
+
+    void endGame();
+
+    void getCaracter();
+
+    void getMap();
+
+    
+
+}
