57127747b33d79430fbd84ebb10870788c040475
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-04-03 12:16:13 +0200

    created class BombarderoController

---
+++ src/main/java/it/unibo/bombardero/core/impl/BombarderoController.java
@@ -0,0 +1,31 @@
+package it.unibo.bombardero.core.impl;
+
+import it.unibo.bombardero.core.api.Controller;
+
+public class BombarderoController implements Controller{
+
+    @Override
+    public void startGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startGame'");
+    }
+
+    @Override
+    public void getMainPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMainPlayer'");
+    }
+
+    @Override
+    public void getEnemies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEnemies'");
+    }
+
+    @Override
+    public void getMap() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMap'");
+    }
+    
+}
