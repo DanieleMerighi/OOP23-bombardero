9f42c05f302f1c9f5d314993802218a0a21a1022
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-04 11:52:12 +0200

    Corrections

--- src/main/java/it/unibo/bombardero/cell/Bomb.java
+++ src/main/java/it/unibo/bombardero/cell/Bomb.java
@@ -4,14 +4,15 @@ import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.map.api.Pair;
 
 public class Bomb extends Cell {
+
     private int range;
-    private static int maxRange;
+    private final static int MAX_RANGE = 3; // TO-DO: decide the max bomb range
     private GameManager mgr;
     private Pair pos;
 
     public Bomb(GameManager mgr, Pair pos){
-        this.mgr=mgr;
-        this.pos=pos;
+        this.mgr = mgr;
+        this.pos = pos;
     }
 
     void update(){}
