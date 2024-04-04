eca33308f9c34d9096c91be2b0f2e0a7b568f493
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-04-04 11:45:53 +0200

    first implementation of BombarderoCollision

---
+++ src/main/java/it/unibo/bombardero/physics/impl/BombarderoCollision.java
@@ -0,0 +1,19 @@
+package it.unibo.bombardero.physics.impl;
+
+import it.unibo.bombardero.physics.api.CollisionEngine;
+
+public class BombarderoCollision implements CollisionEngine{
+
+    @Override
+    public void computeFlame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'computeFlame'");
+    }
+
+    @Override
+    public void checkFlameCollision() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkFlameCollision'");
+    }
+    
+}
