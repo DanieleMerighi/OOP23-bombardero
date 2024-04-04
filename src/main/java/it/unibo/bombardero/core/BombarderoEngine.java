5a056c4243cfcec87b026202c9569422b9b6cdd5
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-04 10:03:35 +0200

    Implementation of constructor and initGame methods

--- src/main/java/it/unibo/bombardero/core/BombarderoEngine.java
+++ src/main/java/it/unibo/bombardero/core/BombarderoEngine.java
@@ -1,6 +1,8 @@
 package it.unibo.bombardero.core;
 
+import it.unibo.bombardero.core.api.Controller;
 import it.unibo.bombardero.core.api.GameManager;
+import it.unibo.bombardero.core.impl.BombarderoGameManager;
 import it.unibo.bombardero.view.BombarderoGraphics;
 
 public class BombarderoEngine {
@@ -8,18 +10,19 @@ public class BombarderoEngine {
     private final static long sleepTime = 16l; // Time during which the thread sleeps, equivalent to about 60FPS
     private GameManager gameManager;
     private BombarderoGraphics graphics;
+    private Controller controller;
 
-    public BombarderoEngine(GameManager manager, BombarderoGraphics graphics) {
-        this.gameManager = manager;
+    public BombarderoEngine(Controller controller, BombarderoGraphics graphics) {
+        this.controller = controller;
         this.graphics = graphics;
     }
 
     public void initGame() {
-        
+        this.gameManager = new BombarderoGameManager(controller);
     }   
     
     public void mainLoop() {
-
+        
     }
 
     private void waitForNextFrame(long currentCycleStartTime) {
