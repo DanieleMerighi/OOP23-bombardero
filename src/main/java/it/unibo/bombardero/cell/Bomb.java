076d3ad977cdd9ce781e6c257e2d229c140fd079
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-04-04 12:09:19 +0200

    Creted enum for type of bomb in Bomb class

--- src/main/java/it/unibo/bombardero/cell/Bomb.java
+++ src/main/java/it/unibo/bombardero/cell/Bomb.java
@@ -5,10 +5,28 @@ import it.unibo.bombardero.map.api.Pair;
 
 public class Bomb extends Cell {
 
+    public enum BombType{
+        
+        BASIC("basic"),
+        PIERCING("piescing"),
+        REMOTE("remote");
+
+        private String type;
+
+        private BombType(String type){
+            this.type=type;
+        }
+
+        public String getType(){
+            return type;
+        }
+    }
+
     private int range;
     private final static int MAX_RANGE = 3; // TO-DO: decide the max bomb range
     private GameManager mgr;
     private Pair pos;
+    private 
 
     public Bomb(GameManager mgr, Pair pos){
         this.mgr = mgr;
