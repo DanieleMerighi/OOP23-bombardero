8d4c5b03b8fc2f7c1eecaa03131d55e4b79adb45
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-04-04 11:18:00 +0200

    Implementation a enum for type of flame

--- src/main/java/it/unibo/bombardero/cell/Flame.java
+++ src/main/java/it/unibo/bombardero/cell/Flame.java
@@ -1,5 +1,30 @@
 package it.unibo.bombardero.cell;
 
 public class Flame extends Cell {
+    private FlameType flameType;
     
+    public enum FlameType {
+
+        CROSS("cross"),
+        BODY_VERTICAL("vertical"),
+        BODY_HORIZONTAL("horizontal"),
+        END_TOP("end_top"),
+        END_BOTTOM("end_bottom"),
+        END_RIGHT("end_right"),
+        END_LEFT("end_left");
+
+        private String type;
+
+        private FlameType(final String type) {
+            this.type = type;
+        }
+
+        public String getTypeName() {
+            return this.type;
+        }
+    }
+
+    public Flame(FlameType type) {
+        this.flameType = type;
+    }
 }
