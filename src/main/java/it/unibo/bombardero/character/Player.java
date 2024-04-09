437403e362a44d2a9dfaa33753235f983ad2e2a2
Daniele Merighi <daniele.merighi2@studio.unibo.it>
2024-04-09 17:57:49 +0200

    Adjust Player position

--- src/main/java/it/unibo/bombardero/character/Player.java
+++ src/main/java/it/unibo/bombardero/character/Player.java
@@ -1,12 +1,11 @@
 package it.unibo.bombardero.character;
 
 import it.unibo.bombardero.core.api.GameManager;
-import it.unibo.bombardero.map.api.Pair;
 
 public class Player extends Character {
 
-    public Player(GameManager manager, Pair coord, int width, int height) {
-        super(manager, coord, width, height);
+    public Player(GameManager manager, float x, float y, int width, int height) {
+        super(manager, x, y, width, height);
     }
 
     public void update() {
