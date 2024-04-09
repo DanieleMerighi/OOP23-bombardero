cef0dd6296d1a8be946870387957323e9a8aca23
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-04-09 11:51:52 +0200

    added constructur

--- src/main/java/it/unibo/bombardero/character/Player.java
+++ src/main/java/it/unibo/bombardero/character/Player.java
@@ -1,5 +1,14 @@
 package it.unibo.bombardero.character;
 
-public class Player {
+import it.unibo.bombardero.map.api.Pair;
+
+public class Player extends Character {
+
+    public Player(Pair coord, int width, int height) {
+        super(coord, width, height);
+    }
+
+    public void update() {
+    }
     
 }
