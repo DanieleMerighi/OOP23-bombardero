5bdb77d5a5e4c4099418b3e1b6fff49898e2ad00
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-16 11:55:29 +0200

    Added interface comments

--- src/main/java/it/unibo/bombardero/map/api/GameMap.java
+++ src/main/java/it/unibo/bombardero/map/api/GameMap.java
@@ -12,26 +12,77 @@ import it.unibo.bombardero.cell.Flame;
  */
 public interface GameMap {
 
+    /**
+     * Updates the dynamic aspect of the map, calling the {@link void#update()} method of
+     * MapManager
+     */
     void update();
 
+    /**
+     * Adds a bomb's reference to the map
+     * @param bomb the bomb to be added
+     * @param coordinate where the bomb is to be placed
+     */
     void addBomb(final Bomb bomb, Pair coordinate);
     
+    /**
+     * Adds a flame's reference to the map
+     * @param flame the flame to be added
+     * @param coordinate where the flame is to be placed
+     */
     void addFlame(Flame flame, Pair coordinate);
 
+    /** 
+     * Creates and adds an unbreakable wall to the map
+     * @param coordinate where the wall is to be placed
+     */
     void addUnbreakableWall(Pair coordinate);
 
+    /**
+     * Creates and adds a breakable wall to the map
+     * @param coordinate where the wall is to be placed
+     */
     void addBreakableWall(Pair coordinate);
 
+    /** 
+     * Returns true if the cell at the coordinate is a Bomb
+     * @param coordinate the coordinate to check 
+     * @return wether the cell at the coordinate is a Bomb
+     */
     boolean isBomb(Pair coordinate);
 
+    /**
+     * Returns true if the cell at the coordinate is a Breakable Wall
+     * @param coordinate the coordinate to check 
+     * @return wether the cell at the coordinate is a Breakable Wall
+     */
     boolean isBreakableWall(Pair coordinate);
 
+    /**
+     * Returns true if the cell at the coordinate is an Unbreakable Wall
+     * @param coordinate the coordinate to check 
+     * @return wether the cell at the coordinate is an Unbreakable Wall
+     */
     boolean isUnbreakableWall(Pair coordinate);
 
+    /**
+     * Returns true if the cell at the coordinate is a Flame
+     * @param coordinate the coordinate to check 
+     * @return wether the cell at the coordinate is a Flame
+     */
     boolean isFlame(Pair coordinate);
 
+    /**
+     * Returns true if the cell at the coordinate is empty
+     * @param coordinate the coordinate to check 
+     * @return wether the cell is empty or not
+     */
     boolean isEmpty(Pair coordinate);
 
+    /**
+     * Returns a copy of the Map used to map the coordinates to the elements of the game
+     * @return a copy of the map
+     */
     Map<Pair, Cell> getMap();
 
 }
