1a055e3b3847b351de73b6a660ec2d1588eae052
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-23 20:33:19 +0200

    Stored reference of the game manager, implementation of getMap method

--- src/main/java/it/unibo/bombardero/core/impl/BombarderoController.java
+++ src/main/java/it/unibo/bombardero/core/impl/BombarderoController.java
@@ -1,21 +1,27 @@
 package it.unibo.bombardero.core.impl;
 
+import java.util.Map;
+
+import it.unibo.bombardero.cell.Cell;
 import it.unibo.bombardero.core.BombarderoEngine;
 import it.unibo.bombardero.core.api.Controller;
+import it.unibo.bombardero.core.api.GameManager;
+import it.unibo.bombardero.map.api.Pair;
 import it.unibo.bombardero.view.BombarderoGraphics;
 
 public class BombarderoController implements Controller {
 
-    private BombarderoGraphics graphics;
+    private final BombarderoGraphics graphics;
+    private GameManager manager;
 
-    public BombarderoController(BombarderoGraphics graphics) {
+    public BombarderoController(final BombarderoGraphics graphics) {
         this.graphics = graphics;
     }
 
     @Override
     public void startGame() {
         BombarderoEngine engine = new BombarderoEngine(this, this.graphics);
-        engine.initGameManager();
+        this.manager = engine.initGameManager();
         engine.mainLoop();
     }
 
@@ -32,9 +38,8 @@ public class BombarderoController implements Controller {
     }
 
     @Override
-    public void getMap() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMap'");
+    public Map<Pair, Cell> getMap() {
+        return manager.getGameMap().getMap();
     }
     
 }
