7efd57443b1c777738e16510e539dbdc5b7929eb
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-26 10:51:23 +0200

    Remove debug lines

--- src/main/java/it/unibo/bombardero/Bombardero.java
+++ src/main/java/it/unibo/bombardero/Bombardero.java
@@ -8,13 +8,6 @@ import it.unibo.bombardero.view.BombarderoGraphics;
 public class Bombardero {
     
     public static void main(String[] args) {
-        /* Baga: debug lines so i can understand the window size when someone runs the game */
-        System.out.println(
-            "Max bounds: " + GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds()
-        );
-        System.out.println(
-            "toolkit size: " + Toolkit.getDefaultToolkit().getScreenSize()
-        );
         new BombarderoGraphics();
     }
 }
