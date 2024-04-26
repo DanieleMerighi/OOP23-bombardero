e2dc54f7c0760e3b3a1bc976af9f0d3fe6a41d6d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-26 10:35:48 +0200

    Removed unused imports

--- src/main/java/it/unibo/bombardero/Bombardero.java
+++ src/main/java/it/unibo/bombardero/Bombardero.java
@@ -2,12 +2,9 @@ package it.unibo.bombardero;
 
 import it.unibo.bombardero.view.BombarderoGraphics;
 
-import java.awt.GraphicsEnvironment;
-
 public class Bombardero {
     
     public static void main(String[] args) {
-        System.out.println(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
         new BombarderoGraphics();
     }
 }
