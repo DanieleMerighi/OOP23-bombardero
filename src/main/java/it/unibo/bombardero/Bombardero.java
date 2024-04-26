dad73486827cbf6f73fd6ffe39f3811478cc66ed
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-26 10:04:48 +0200

    Test commit

--- src/main/java/it/unibo/bombardero/Bombardero.java
+++ src/main/java/it/unibo/bombardero/Bombardero.java
@@ -1,13 +1,13 @@
 package it.unibo.bombardero;
 
-import it.unibo.bombardero.core.api.Controller;
-import it.unibo.bombardero.core.impl.BombarderoController;
-import it.unibo.bombardero.core.impl.BombarderoGameManager;
 import it.unibo.bombardero.view.BombarderoGraphics;
+
+import java.awt.GraphicsEnvironment;
 
 public class Bombardero {
     
     public static void main(String[] args) {
+        System.out.println(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
         new BombarderoGraphics();
     }
 }
