76859ce4053b05ce027f6d177cd4f0f0045c9c28
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-04-26 10:39:56 +0200

    Test commit

--- src/main/java/it/unibo/bombardero/Bombardero.java
+++ src/main/java/it/unibo/bombardero/Bombardero.java
@@ -1,10 +1,19 @@
 package it.unibo.bombardero;
 
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
 import it.unibo.bombardero.view.BombarderoGraphics;
 
 public class Bombardero {
     
     public static void main(String[] args) {
+        System.out.println(
+            "Max bounds: " + GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds()
+        );
+        System.out.println(
+            "toolkit size: " + Toolkit.getDefaultToolkit().getScreenSize()
+        );
         new BombarderoGraphics();
     }
 }
