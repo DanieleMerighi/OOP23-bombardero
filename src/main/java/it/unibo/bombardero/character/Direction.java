5443b585d11176eac140c2652b12dbad6f926d04
Daniele Merighi <daniele.merighi2@studio.unibo.it>
2024-04-27 19:58:57 +0200

    Fixed row and col

--- src/main/java/it/unibo/bombardero/character/Direction.java
+++ src/main/java/it/unibo/bombardero/character/Direction.java
@@ -1,10 +1,10 @@
 package it.unibo.bombardero.character;
 
 public enum Direction {
-    UP(0, -1),
-    DOWN(0, 1),
-    LEFT(-1, 0),
-    RIGHT(1, 0);
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1);
 
     private final int dx;
     private final int dy;
