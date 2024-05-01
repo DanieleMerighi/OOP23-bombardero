b15cdf19d827f776a0a84f49d47ffc7cf34156d5
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-05-01 10:44:29 +0200

    Added strings for image resources

--- src/main/java/it/unibo/bombardero/cell/Flame.java
+++ src/main/java/it/unibo/bombardero/cell/Flame.java
@@ -4,13 +4,13 @@ public class Flame extends Cell {
 
     public enum FlameType {
 
-        FLAME_CROSS("cross"),
-        FLAME_BODY_VERTICAL("vertical"),
-        FLAME_BODY_HORIZONTAL("horizontal"),
-        FLAME_END_TOP("end_top"),
-        FLAME_END_BOTTOM("end_bottom"),
-        FLAME_END_RIGHT("end_right"),
-        FLAME_END_LEFT("end_left");
+        FLAME_CROSS("explosion/cross"),
+        FLAME_BODY_VERTICAL("explosion/body/vert"),
+        FLAME_BODY_HORIZONTAL("explosion/body/hor"),
+        FLAME_END_TOP("explosion/end/up"),
+        FLAME_END_BOTTOM("explosion/end/down"),
+        FLAME_END_RIGHT("explosion/end/right"),
+        FLAME_END_LEFT("explosion/end/left");
 
         private String typeString;
 
