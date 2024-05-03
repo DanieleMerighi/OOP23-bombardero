132893a843289fdbbe81e18edd1f1ab3950c77e9
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-05-03 11:43:29 +0200

    added 2 static method that return the body and end type of flames based on the direction

--- src/main/java/it/unibo/bombardero/cell/Flame.java
+++ src/main/java/it/unibo/bombardero/cell/Flame.java
@@ -1,5 +1,10 @@
 package it.unibo.bombardero.cell;
 
+import java.util.Map;
+import java.util.Set;
+
+import it.unibo.bombardero.character.Direction;
+
 public class Flame extends Cell {
 
     public enum FlameType {
@@ -14,12 +19,32 @@ public class Flame extends Cell {
 
         private String typeString;
 
+        private static final Map<Direction,FlameType> FLAME_BODY_TYPES_MAP = Map.of(
+        Direction.LEFT, FlameType.FLAME_BODY_HORIZONTAL,
+        Direction.RIGHT, FlameType.FLAME_BODY_HORIZONTAL,
+        Direction.UP, FlameType.FLAME_BODY_VERTICAL,
+        Direction.DOWN, FlameType.FLAME_BODY_VERTICAL);
+        
+        private static final Map<Direction, FlameType> FLAME_END_TYPES_MAP = Map.of(
+        Direction.LEFT, FlameType.FLAME_END_LEFT,
+        Direction.RIGHT, FlameType.FLAME_END_RIGHT,
+        Direction.UP, FlameType.FLAME_END_TOP,
+        Direction.DOWN, FlameType.FLAME_END_BOTTOM);
+
         private FlameType(final String typeString) {
             this.typeString = typeString;
         }
 
         public String getTypeString() {
             return this.typeString;
+        }
+
+        public static FlameType getFlameBodyType(Direction dir) {
+            return FLAME_BODY_TYPES_MAP.get(dir);
+        }
+
+        public static FlameType getFlameEndType(Direction dir) {
+            return FLAME_END_TYPES_MAP.get(dir);
         }
 
     }
