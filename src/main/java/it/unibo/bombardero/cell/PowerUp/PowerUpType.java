6ce6fb68161960d4d2aefe05545b67cb761a9923
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-05-12 19:27:53 +0200

    Adding plus and minus bomb powerup. Renamed powerups

--- src/main/java/it/unibo/bombardero/cell/PowerUp/PowerUpType.java
+++ src/main/java/it/unibo/bombardero/cell/PowerUp/PowerUpType.java
@@ -4,14 +4,17 @@ public enum PowerUpType {
     // Bomb type-related P.U.
     REMOTE_BOMB("powerup/bomb_remote"),
     PIERCING_BOMB("powerup/bomb_piercing"),
-    POWER_BOMB("powerup/bomb_hyper"),
+    POWER_BOMB("powerup/bomb_power"),
+    // Number of bomb-related P.U.
+    PLUS_ONE_BOMB("powerup/bomb_plus_one"),
+    MINUS_UNE_BOMB("powerup/bomb_minus_one"),
     // Bomb range-related P.U.
-    PLUSONE_BOMBRANGE("powerup/fire_plus_one"),
-    MINUSONE_BOMBRANGE("powerup/fire_minus_one"),
-    MAX_BOMBRANGE("powerup/fire_max"),
+    PLUS_ONE_FLAME_RANGE("powerup/flame_plus_one"),
+    MINUS_ONE_FLAME_RANGE("powerup/flame_minus_one"),
+    MAX_FLAME_RANGE("powerup/flame_max"),
     // Speed-related P.U.
-    PLUSONE_SKATES("powerup/skates_plus_one"),
-    MINUSONE_SKATES("powerup/skates_minus_one"),
+    PLUS_ONE_SKATES("powerup/skates_plus_one"),
+    MINUS_ONE_SKATES("powerup/skates_minus_one"),
     // Botton-related P.U.
     PUNCH("powerup/punch"),
     LINE_BOMB("powerup/line"),
