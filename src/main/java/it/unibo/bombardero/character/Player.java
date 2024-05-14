f6e7b2ef50bac9a1653c3cc4eb75d9879d6b0e79
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-05-14 10:11:32 +0200

    Transfering player's game attribute to the character. Adding player's method

--- src/main/java/it/unibo/bombardero/character/Player.java
+++ src/main/java/it/unibo/bombardero/character/Player.java
@@ -2,28 +2,45 @@ package it.unibo.bombardero.character;
 
 import java.util.Optional;
 import it.unibo.bombardero.cell.PowerUp.PowerUpType;
+import it.unibo.bombardero.core.KeyboardInput;
 import it.unibo.bombardero.core.api.GameManager;
+import it.unibo.bombardero.map.api.Coord;
+import it.unibo.bombardero.cell.Bomb;
+import it.unibo.bombardero.cell.BombFactory;
+import it.unibo.bombardero.cell.Bomb.BombType;
 
 public class Player extends Character {
 
-    //game attribute
-    private int numBomb;
-    private int flameRange;
-    private int speed;
-    private Optional<PowerUpType> bombType;
-    private Optional<PowerUpType> kick;
-    private Optional<PowerUpType> skull;
+    //private Optional<PowerUpType> skull;
 
     //view attribute
-    private int facingDirection;
+    //private int facingDirection;
 
 
-    public Player(GameManager manager, float x, float y, int width, int height) {
-        super(manager, x, y, width, height);
+    public Player(GameManager manager, Coord coord, int width, int height) {
+        super(manager, coord, width, height);
     }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void placeBomb(){
+        if (hasBombsLeft()) {
+            /*if(manager.addBomb(BombFactory.CreateBomb(bombType, getCoord(), flameRange))
+                numBomb--;   */
+        }
+    }
+
+    public boolean hasBombsLeft(){
+        return this.numBomb>0;
+    }
+
+
 
     public void update() {
         //based on which key get pressed, the player moves in that direction
+        
     }
     
 }
