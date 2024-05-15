b7e43475551d1d524d973661d783df71b27cf812
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-05-15 22:35:11 +0200

    Modified the sprite to support sprites with different frames number

--- src/main/java/it/unibo/bombardero/view/BombarderoSprite.java
+++ src/main/java/it/unibo/bombardero/view/BombarderoSprite.java
@@ -2,6 +2,8 @@ package it.unibo.bombardero.view;
 
 import java.awt.Image;
 
+import it.unibo.bombardero.character.Direction;
+
 /* This class represents a single instance of a animated cell, that updates itself 
  * and returns an image when requested
  */
@@ -12,8 +14,8 @@ public class BombarderoSprite {
     private int counter = 0;
     private int currentFrame = 0;
 
-    public BombarderoSprite(final String resource, final ResourceGetter rg, final int frames) {
-        FRAMES_PER_SPRITE = frames;
+    public BombarderoSprite(final String resource, final ResourceGetter rg, final Direction facingDirection) {
+        FRAMES_PER_SPRITE = getFramesFromPosition(facingDirection);
         asset = new Image[FRAMES_PER_SPRITE];
         for(int i = 0; i < FRAMES_PER_SPRITE; i++) {
             asset[i] = rg.loadImage(resource + Integer.toString(i));
@@ -30,6 +32,13 @@ public class BombarderoSprite {
     public Image getImage() {
         /* QUANDO ANDRA' LA VERSIONE NAIVE, PROVARE: (int)Math.ceil(counter / FRAMES_PER_SPRITE) */
         return asset[currentFrame];
+    }
+
+    private int getFramesFromPosition(Direction facingDirection) {
+        if(facingDirection.equals(Direction.UP) || facingDirection.equals(Direction.DOWN)) {
+            return 2;
+        }
+        return 4;
     }
 
 }
