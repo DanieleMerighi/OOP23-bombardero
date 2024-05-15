b4d541b8ebe33b3eee60824351402877e0356bc3
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-05-15 13:06:09 +0200

    Implemented the player movement with the relatives javadoc comments

--- src/main/java/it/unibo/bombardero/character/Player.java
+++ src/main/java/it/unibo/bombardero/character/Player.java
@@ -6,21 +6,18 @@ import it.unibo.bombardero.cell.BombFactory;
 
 /**
  * This class represents a Player in the game.
- * This class extends the Character class and is responsible for handling 
+ * This class extends the Character class and is responsible for handling
  * player-specific actions and behaviors.
  * 
- * Author: Jacopo Turchi
+ * @Author: Jacopo Turchi
  */
 public class Player extends Character {
-
-    // view attribute
-    // private int facingDirection;
 
     /**
      * Constructs a new Player instance with the specified parameters.
      * 
-     * @param manager the game manager that controls the game state
-     * @param coord the initial coordinates where the player is spawned
+     * @param manager     the game manager that controls the game state
+     * @param coord       the initial coordinates where the player is spawned
      * @param bombFactory the factory to create bombs
      */
     public Player(final GameManager manager, final Coord coord, final BombFactory bombFactory) {
@@ -29,10 +26,25 @@ public class Player extends Character {
 
     /**
      * Updates the Player's interactions with the map.
-     * This method should define how the player moves and interacts based on user input.
+     * This method should define how the player moves and interacts based on user
+     * input.
      */
     @Override
     public void update() {
+        // Player movement:
+        setCharacterPosition(computeNewPlayerPosition());
+    }
 
+    /**
+     * Sums the current Player position with the direction recived in imput
+     * Uses getSpeed to determine how much the Player needs to move in the facing
+     * direction
+     * 
+     * @return the new updated Player position
+     */
+    private Coord computeNewPlayerPosition() {
+        return getCharacterPosition()
+                .sum(new Coord((getFacingDirection().getDx()) * getSpeed(),
+                        (getFacingDirection().getDy()) * getSpeed()));
     }
 }
