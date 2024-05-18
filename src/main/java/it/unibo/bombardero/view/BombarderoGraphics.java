e1fbfc0bf1e67457d6fbfcbf162a6ce0178cde69
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-05-18 15:21:16 +0200

    Implementation of the Card Layout

--- src/main/java/it/unibo/bombardero/view/BombarderoGraphics.java
+++ src/main/java/it/unibo/bombardero/view/BombarderoGraphics.java
@@ -1,22 +1,13 @@
 package it.unibo.bombardero.view;
 
 import java.awt.CardLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.image.BufferedImage;
-import java.awt.GraphicsEnvironment;
 import java.awt.Image;
-import java.awt.Toolkit;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-import org.jgrapht.alg.spanning.EsauWilliamsCapacitatedMinimumSpanningTree;
 
 import it.unibo.bombardero.core.KeyboardInput;
 import it.unibo.bombardero.core.api.Controller;
@@ -28,28 +19,28 @@ import it.unibo.bombardero.core.impl.BombarderoController;
  */
 public class BombarderoGraphics {
 
+    public final static String MENU_CARD = "menu";
+    public final static String END_CARD = "end";
+    public final static String GAME_CARD = "game";
+
     private Controller controller;
-    // adding the keyboard input
-    private final KeyboardInput keyInput;
 
     private final ResourceGetter resourceGetter = new ResourceGetter();
     private final ResizingEngine resizingEngine = new ResizingEngine();
     private final BufferedImage game_icon = resourceGetter.loadImage("icons/icon_happy");
 
     private final JFrame frame; 
-    private JPanel deck;
-    private CardLayout layout;
+    private final JPanel deck;
+    private final CardLayout layout = new CardLayout();
 
-    private final GameCard gameCard;
-    private final GameoverCard endGameCard;
+    private GameCard gameCard;
+    private GameoverCard endGameCard;
     private final MenuCard menuCard;
     
     public BombarderoGraphics() {
         this.controller = new BombarderoController(this);
-        this.keyInput = new KeyboardInput(this.controller); // passing the controller reference to the keyboard input
         this.frame = new JFrame("Bombardero: the Bomberman remake");
-        this.deck = new JPanel(new CardLayout());
-        this.layout = (CardLayout)deck.getLayout();
+        this.deck = new JPanel(layout);
         
         frame.pack(); // calling pack on the frame generates the insets
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,10 +48,12 @@ public class BombarderoGraphics {
         frame.setResizable(true);
         frame.setLocationRelativeTo(null);
         frame.setIconImage(game_icon.getScaledInstance(64, 64, Image.SCALE_SMOOTH));
-        
-        this.gameCard = new GameCard(frame, resizingEngine, controller);
-        this.endGameCard = new GameoverCard();
-        this.menuCard = new MenuCard(resourceGetter);
+
+        this.menuCard = new MenuCard(controller, this, resourceGetter);
+        deck.add(MENU_CARD, menuCard);
+        layout.addLayoutComponent(menuCard, MENU_CARD);
+        deck.validate();
+        System.out.println("Menu card initialized");
 
         /* This listener calls for the ResizingEngine to dinamically update the 
         * frame's size when it is resized, see the implementation for more */
@@ -69,11 +62,30 @@ public class BombarderoGraphics {
                 frame.setSize(resizingEngine.getNewWindowSize(frame));
             }
         });
-
         // Recives keyboard input
-        frame.addKeyListener(keyInput);
-
-        frame.add(menuCard);
+        frame.add(deck);
+        showCard(MENU_CARD);
         this.frame.setVisible(true);
+    }
+
+    public void showCard(final String cardName) {
+        layout.show(deck, cardName);
+        System.out.println("\"" + cardName + "\"" + " card showed");
+    }
+
+    public void initGameCard() {
+        this.gameCard = new GameCard(frame, resizingEngine, controller);
+        this.endGameCard = new GameoverCard();
+        deck.add(GAME_CARD, gameCard);
+        deck.add(END_CARD, endGameCard);
+        layout.addLayoutComponent(gameCard, GAME_CARD);
+        layout.addLayoutComponent(endGameCard, END_CARD);
+        deck.validate();
+        System.out.println("Game card initialized");
+    }
+
+    public void update() {
+        gameCard.repaint(1);
+        System.out.println("Graphics updated");
     }
 }
