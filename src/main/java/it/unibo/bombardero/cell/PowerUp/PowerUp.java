1c2fbd93c541c58acd716457bbb15684c98bbea7
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-05-18 15:25:45 +0200

    Adding getType and applyEffect to PowerUp

--- src/main/java/it/unibo/bombardero/cell/PowerUp/PowerUp.java
+++ src/main/java/it/unibo/bombardero/cell/PowerUp/PowerUp.java
@@ -1,11 +1,27 @@
 package it.unibo.bombardero.cell.PowerUp;
 
+import java.util.function.Consumer;
+
+import it.unibo.bombardero.character.Character;
 import it.unibo.bombardero.cell.Cell;
 
 public class PowerUp extends Cell {
+    private PowerUpType type;
+    private Consumer<Character> effect;
 
-    public PowerUp(CellType type) {
-        super(type);
+    public PowerUp(final PowerUpType type, final Consumer<Character> effect) {
+        super(CellType.POWERUP);
+        this.type = type;
+        this.effect = effect;
     }
+
+    public PowerUpType getType() {
+        return type;
+    }
+
+    public void applyEffect(Character character) {
+        effect.accept(character);
+    }
+
     
 }
