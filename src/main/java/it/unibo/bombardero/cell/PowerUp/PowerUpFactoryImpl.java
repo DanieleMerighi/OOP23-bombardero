5286bb9de8f6228ad3b87db6941c0ae661f79039
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-05-18 16:28:34 +0200

    Modified getWeightedPowerUpList method

--- src/main/java/it/unibo/bombardero/cell/PowerUp/PowerUpFactoryImpl.java
+++ src/main/java/it/unibo/bombardero/cell/PowerUp/PowerUpFactoryImpl.java
@@ -1,9 +1,11 @@
 package it.unibo.bombardero.cell.PowerUp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import org.apache.commons.math3.distribution.EnumeratedDistribution;
 import org.apache.commons.math3.util.Pair;
@@ -20,66 +22,62 @@ public class PowerUpFactoryImpl implements PowerUpFactory {
 
     }
 
-    // extract a random weighted PowerUp
+    // Extract a random weighted PowerUp
     private PowerUpType extractPowerUp() {
         return new EnumeratedDistribution<>(getWeightedPowerUpList()).sample();
     }
 
+    // Generate a list of power-ups with their weights
     private List<Pair<PowerUpType, Double>> getWeightedPowerUpList() {
-        final List<Pair<PowerUpType, Double>> powerupList = new ArrayList<Pair<PowerUpType, Double>>();
-
-        for (PowerUpType type : PowerUpType.values()) {
-            powerupList.add(new Pair<PowerUpType, Double>(type, type.getWeight()));
-        }
-        return powerupList;
+        return Arrays.stream(PowerUpType.values())
+                .map(type -> new Pair<>(type, type.getWeight()))
+                .collect(Collectors.toList());
     }
 
-    public Consumer<Character> createEffect(final PowerUpType powerUpType){
+    public Consumer<Character> createEffect(final PowerUpType powerUpType) {
         Consumer<Character> lambda;
         switch (powerUpType) {
             case REMOTE_BOMB ->
-                lambda =  character -> character.setBombType(Optional.of(PowerUpType.REMOTE_BOMB));
+                lambda = character -> character.setBombType(Optional.of(PowerUpType.REMOTE_BOMB));
             case PIERCING_BOMB ->
-                lambda =  character -> character.setBombType(Optional.of(PowerUpType.PIERCING_BOMB));
-            case POWER_BOMB->
-                lambda =  character -> character.setBombType(Optional.of(PowerUpType.POWER_BOMB));
-            case PLUS_ONE_BOMB->
-                lambda =  character -> character.increaseNumBomb();
-            case MINUS_ONE_BOMB->
-                lambda =  character -> character.decreaseNumBomb();
-            case PLUS_ONE_FLAME_RANGE->
-                lambda =  character -> character.increaseFlameRange();
-            case MINUS_ONE_FLAME_RANGE->
-                lambda =  character -> character.decreaseFlameRange();
-            case MAX_FLAME_RANGE->
-                lambda =  character -> character.setFlameRange(Character.MAX_FLAME_RANGE);
-            case PLUS_ONE_SKATES->
-                lambda =  character -> character.increaseSpeed();
-            case MINUS_ONE_SKATES->
-                lambda =  character -> character.decreaseSpeed();
-            case LINE_BOMB->
-                lambda =  character -> character.setLineBomb(true);
-            case KICK->
-                lambda =  character -> character.setKick(true);
-            case SKULL->
-                {
-                    List<Consumer<Character>> skull = new ArrayList<>();
-                    skull.add(character -> {
-                        float startingSpeed = character.getSpeed();
-                        character.setSpeed(0);
-                        //prendi actual time = timer
-                        //if actual time - timer >= 10
-                        character.setSpeed(startingSpeed);
-                    }
-                    );
-                    skull.add(character -> character.setSpeed(10));
-                    skull.add(character -> character.setSpeed(0));
-                    lambda = skull.get(0);
+                lambda = character -> character.setBombType(Optional.of(PowerUpType.PIERCING_BOMB));
+            case POWER_BOMB ->
+                lambda = character -> character.setBombType(Optional.of(PowerUpType.POWER_BOMB));
+            case PLUS_ONE_BOMB ->
+                lambda = character -> character.increaseNumBomb();
+            case MINUS_ONE_BOMB ->
+                lambda = character -> character.decreaseNumBomb();
+            case PLUS_ONE_FLAME_RANGE ->
+                lambda = character -> character.increaseFlameRange();
+            case MINUS_ONE_FLAME_RANGE ->
+                lambda = character -> character.decreaseFlameRange();
+            case MAX_FLAME_RANGE ->
+                lambda = character -> character.setFlameRange(Character.MAX_FLAME_RANGE);
+            case PLUS_ONE_SKATES ->
+                lambda = character -> character.increaseSpeed();
+            case MINUS_ONE_SKATES ->
+                lambda = character -> character.decreaseSpeed();
+            case LINE_BOMB ->
+                lambda = character -> character.setLineBomb(true);
+            case KICK ->
+                lambda = character -> character.setKick(true);
+            case SKULL -> {
+                List<Consumer<Character>> skull = new ArrayList<>();
+                skull.add(character -> {
+                    float startingSpeed = character.getSpeed();
+                    character.setSpeed(0);
+                    // prendi actual time = timer
+                    // if actual time - timer >= 10
+                    character.setSpeed(startingSpeed);
+                });
+                skull.add(character -> character.setSpeed(10));
+                skull.add(character -> character.setNumBomb(0));
+                lambda = skull.get(0);
             }
-            default->
+            default ->
                 throw new IllegalArgumentException("Unknown power-up type: " + powerUpType);
         }
         return lambda;
-        
+
     }
 }
