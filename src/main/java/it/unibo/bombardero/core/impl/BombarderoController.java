86fa3f74d6cee95c87dd578541811755aad07c02
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-05-18 15:19:52 +0200

    Implemented pauseGame and endGame methods

--- src/main/java/it/unibo/bombardero/core/impl/BombarderoController.java
+++ src/main/java/it/unibo/bombardero/core/impl/BombarderoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import it.unibo.bombardero.cell.Cell;
 import it.unibo.bombardero.core.BombarderoEngine;
 import it.unibo.bombardero.core.api.Controller;
+import it.unibo.bombardero.core.api.Engine;
 import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.map.api.Pair;
 import it.unibo.bombardero.view.BombarderoGraphics;
@@ -15,6 +16,7 @@ public class BombarderoController implements Controller {
 
     private final BombarderoGraphics graphics;
     private GameManager manager;
+    private Engine engine;
 
     public BombarderoController(final BombarderoGraphics graphics) {
         this.graphics = graphics;
@@ -22,12 +24,23 @@ public class BombarderoController implements Controller {
 
     @Override
     public void startGame() {
-        BombarderoEngine engine = new BombarderoEngine(this, this.graphics);
+        engine = new BombarderoEngine(this, this.graphics);
         this.manager = engine.initGameManager();
-        engine.mainLoop();
+        graphics.initGameCard();
+        graphics.showCard(BombarderoGraphics.GAME_CARD);
+        manager.startTimer();
+        engine.startGameLoop();
     }
 
-    /* TODO: endGame method */
+    @Override
+    public void pauseGame() {
+        engine.pauseGameLoop();
+    }
+
+    @Override
+    public void endGame() {
+        graphics.showCard(BombarderoGraphics.END_CARD);
+    }
 
     @Override
     public Character getMainPlayer() {
