db4cc9f358ff55819a7dd34e45674e48258452d2
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-05-25 19:23:54 +0200

    Added java Doc

--- src/main/java/it/unibo/bombardero/physics/api/BoundingBox.java
+++ src/main/java/it/unibo/bombardero/physics/api/BoundingBox.java
@@ -7,10 +7,29 @@ import it.unibo.bombardero.character.Direction;
 
 public interface BoundingBox {
 
+    /**
+     * move the bounding box in the same position of a character
+     * @param pos position of a character
+     */
     void move(Point2D pos);
 
-    boolean isColliding(Rectangle2D cellBox);
+    /**
+     * @param bBox
+     * @return if this bounding box is colliding with anothe bounding box
+     */
+    boolean isColliding(BoundingBox bBox);
 
-    float distanceOfCollision(Rectangle2D cellBox, Direction dir);
+    /**
+     * 
+     * @param cellBox
+     * @param dir
+     * @return the distance of the collision
+     */
+    float distanceOfCollision(Rectangle2D bBox, Direction dir);
+
+    /**
+     * @return the rectangle that rappresent the bounding box
+     */
+    Rectangle2D getPhysicsBox();
 
 }
--- src/main/java/it/unibo/bombardero/physics/api/CollisionEngine.java
+++ src/main/java/it/unibo/bombardero/physics/api/CollisionEngine.java
@@ -11,8 +11,15 @@ import it.unibo.bombardero.character.Character;
 
 public interface CollisionEngine {
     
+    /**
+     * Check if character are in the same cell of flames
+     */
     void checkFlameCollision();
 
+    /**
+     * check collision for the character with the cells in front of him
+     * @param character 
+     */
     void checkCharacterCollision(Character character);
 
     
