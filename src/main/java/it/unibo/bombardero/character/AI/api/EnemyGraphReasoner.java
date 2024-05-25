2fe92537d014c2938181cf469748aa0ce859c83a
Daniele Merighi <daniele.merighi2@studio.unibo.it>
2024-05-25 12:07:21 +0200

    Refactor GraphBuilder to use GraphBuilderImpl

---
+++ src/main/java/it/unibo/bombardero/character/AI/api/EnemyGraphReasoner.java
@@ -0,0 +1,23 @@
+package it.unibo.bombardero.character.AI.api;
+
+import it.unibo.bombardero.map.api.Pair;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * This interface defines the functionalities for pathfinding and danger zone analysis for enemies
+ * within the game map. It utilizes the JGraphT library to represent the game map as a graph
+ * and performs graph traversals and shortest path calculations.
+ */
+public interface EnemyGraphReasoner {
+    boolean isInDangerZone(Pair enemyCoord, int explRadius);
+
+    boolean isPathBlockedByWalls(Pair startCell, Pair endCell);
+
+    List<Pair> findShortestPathToPlayer(Pair enemyCoord, Pair playerCoord);
+
+    Optional<Pair> findNearestSafeSpace(Pair enemyCoord, int explRadius);
+
+    Optional<Pair> findNearestBomb(Pair enemyCoord);
+}
+
--- src/main/java/it/unibo/bombardero/character/Enemy.java
+++ src/main/java/it/unibo/bombardero/character/Enemy.java
@@ -2,32 +2,48 @@ package it.unibo.bombardero.character;
 
 import java.util.Optional;
 import java.util.List;
-import java.util.LinkedList;
 import java.util.Random;
 
 import it.unibo.bombardero.cell.BombFactory;
-import it.unibo.bombardero.character.AI.EnemyGraphReasoner;
+import it.unibo.bombardero.character.AI.*;
 import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.map.api.Coord;
-import it.unibo.bombardero.map.api.GameMap;
 import it.unibo.bombardero.map.api.Pair;
 import it.unibo.bombardero.utils.Utils;
 
 import java.util.EnumSet;
 import java.util.stream.Collectors;
 
+/**
+ * Represents an enemy character within the game environment.
+ * Enemies are intelligent entities that can move around the map, detect dangers
+ * (bombs), and chase the player. Their behavior is governed by a state machine
+ * with three states: PATROL, CHASE, and ESCAPE.
+ */
 public class Enemy extends Character {
 
     Optional<Pair> nextMove = Optional.empty();
     private State currentState = State.PATROL; // Initial state
     private int movementTimer = 0; // Timer for movement updates
-    private EnemyGraphReasoner graph;
+    private EnemyGraphReasonerImpl graph;
 
+    /*
+     * Constructs a new Enemy with the specified parameters.
+     * 
+     * @param manager     the game manager that controls the game state
+     * @param coord       the initial coordinates where the enemy is spawned
+     * @param bombFactory the factory to create bombs
+     */
     public Enemy(GameManager manager, Coord coord, BombFactory bombFactory) {
         super(manager, coord, bombFactory);
-        graph = new EnemyGraphReasoner(manager.getGameMap());
+        graph = new EnemyGraphReasonerImpl(manager.getGameMap());
     }
 
+    /**
+     * Checks if the player is within the enemy's detection radius using Manhattan distance.
+     *
+     * @return true if the player is within detection radius, false otherwise.
+     */
     private boolean isEnemyClose() {
         Pair enemyCoord = getIntCoordinate();
         Pair playerCoord = this.getManager().getPlayer().getIntCoordinate();
@@ -40,15 +56,10 @@ public class Enemy extends Character {
         return distance <= detectionRadius; // Check if player is within detection radius
     }
 
-    private void placeBomb(Pair targetCell) {
-        if (hasBombsLeft() && isValidCell(targetCell)
-                && this.getManager().getGameMap().isEmpty(targetCell)) {
-            // map.addBomb(null, targetCell);
-            // numBombs--;
-        }
-    }
-
-    // when the enemy doesn't know where to move he choose randomly
+    /**
+     * Chooses a random valid and empty cell as the next move target when the enemy
+     * doesn't have a specific direction to go.
+     */
     private void moveRandomly() {
         Pair currentCoord = getIntCoordinate();
         List<Direction> dirs = EnumSet.allOf(Direction.class)
@@ -68,22 +79,40 @@ public class Enemy extends Character {
         }
     }
 
+    /**
+     * Checks if a given cell is within the valid map boundaries.
+     *
+     * @param cell the cell to be checked
+     * @return true if the cell is within map boundaries, false otherwise.
+     */
     private boolean isValidCell(Pair cell) {
         return cell.row() >= 0 && cell.col() >= 0 && cell.row() <= Utils.MAP_ROWS && cell.col() <= Utils.MAP_COLS;
     }
 
+     /**
+     * Calculates the next move target based on the current enemy state.
+     * This method delegates the behavior to the current state's `execute` method
+     * and potentially updates the `nextMove` target based on danger zone detection
+     * or pathfinding results.
+     */
     private void computeNextDir() {
-        graph = new EnemyGraphReasoner(this.getManager().getGameMap());
+        graph = new EnemyGraphReasonerImpl(this.getManager().getGameMap());
         currentState.execute(this); // Delegate behavior to current state
         nextMove.ifPresent(cell -> {
+            if(graph.isInDangerZone(cell, getFlameRange())) {
+                nextMove = Optional.empty();
+            }
             if (this.getManager().getGameMap().isBreakableWall(cell)) {
-                placeBomb(getIntCoordinate());
+                placeBomb();
                 // change the state of the enemy?
             }
         });
     }
 
-    /* base AI Heuristics */
+    /**
+     * Updates the enemy's position and behavior within the game environment.
+     * This method is called every frame to handle enemy movement and actions.
+     */
     @Override
     public void update() {
         movementTimer += 1;
@@ -102,8 +131,8 @@ public class Enemy extends Character {
             setCharacterPosition(getCharacterPosition().sum(direction.multiply(getSpeed())));
 
             // Check if reached the target cell using a small tolerance
-            if (Math.abs(target.row() - getCharacterPosition().row()) < getSpeed() &&
-                    Math.abs(target.col() - getCharacterPosition().col()) < getSpeed()) {
+            if (Math.abs(target.row() - getCharacterPosition().row()) < getSpeed()/2 &&
+                    Math.abs(target.col() - getCharacterPosition().col()) < getSpeed()/2) {
                 nextMove = Optional.empty(); // Clear target if reached
             }
         }
@@ -122,7 +151,18 @@ public class Enemy extends Character {
         return direction;
     }
 
+    /**
+     * Represents the different behavioral states of an enemy in the game.
+     * Each state defines specific actions the enemy takes based on its current
+     * situation (e.g., danger zone, player proximity).
+     */
     public enum State {
+        /**
+         * In patrol state, the enemy moves randomly within the map.
+         * It checks for dangers (bombs) and nearby players regularly.
+         * - Transitions to ESCAPE if it detects a bomb within its flame range.
+         * - Transitions to CHASE if it detects the player nearby.
+         */
         PATROL {
             @Override
             void execute(Enemy enemy) {
@@ -135,6 +175,12 @@ public class Enemy extends Character {
                 }
             }
         },
+        /**
+         * In chase state, the enemy actively seeks the player.
+         * It finds the shortest path to the player's current position and sets it
+         * as the next move target.
+         * - Transitions back to PATROL if it loses sight of the player.
+         */
         CHASE {
             @Override
             void execute(Enemy enemy) {
@@ -147,10 +193,16 @@ public class Enemy extends Character {
                 }
             }
         },
+        /**
+         * In escape state, the enemy tries to move away from a detected danger zone.
+         * It finds the nearest safe space outside the bomb's explosion radius
+         * and sets it as the next move target.
+         * - Transitions back to PATROL if it reaches a safe space.
+         */
         ESCAPE {
             @Override
             void execute(Enemy enemy) {
-                if (!enemy.graph.isInDangerZone(enemy.getIntCoordinate(), Utils.EXPLOSION_RADIUS)) { // Safe now
+                if (!enemy.graph.isInDangerZone(enemy.getIntCoordinate(), enemy.getFlameRange())) { // Safe now
                     enemy.currentState = State.PATROL;
                 } else {
                     enemy.nextMove = enemy.graph.findNearestSafeSpace(enemy.getIntCoordinate(), enemy.getFlameRange());
@@ -161,17 +213,27 @@ public class Enemy extends Character {
         abstract void execute(Enemy enemy);
     }
 
+    /**
+     * Retrieves the enemy's current target position, if any.
+     * This method returns an Optional object containing the next move target as a
+     * Pair (row, column) if a target exists. Otherwise, it returns an empty
+     * Optional.
+     *
+     * @return An Optional object containing the next move target (Pair) or empty if
+     *         none.
+     */
     public Optional<Pair> getNextMove() {
         return nextMove;
     }
 
+    /**
+     * Retrieves the enemy's current behavioral state.
+     * This method returns the current State enum value representing the enemy's
+     * current behavior (PATROL, CHASE, or ESCAPE).
+     *
+     * @return The current State enum value of the enemy.
+     */
     public State getState() {
         return currentState;
-    }
-
-    @Override
-    public void placeBomb() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'placeBomb'");
     }
 }
