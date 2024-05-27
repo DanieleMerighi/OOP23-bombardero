e182214eb5766b885ccf1a1cf324cf6f98628ecf
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-05-27 23:16:32 +0200

    Commented enemies

--- src/main/java/it/unibo/bombardero/core/impl/BombarderoGameManager.java
+++ src/main/java/it/unibo/bombardero/core/impl/BombarderoGameManager.java
@@ -40,11 +40,16 @@ public class BombarderoGameManager implements GameManager {
         ce = new BombarderoCollision(this);
         bombFactory = new BombFactoryImpl(this, ce);
         this.player = new Player(this, Utils.PLAYER_SPAWNPOINT, bombFactory);
-        Utils.ENEMIES_SPAWNPOINT.forEach(enemyCoord -> enemies.add(new Enemy(this, enemyCoord, bombFactory)));
+        /* Utils.ENEMIES_SPAWNPOINT.forEach(enemyCoord -> enemies.add(new Enemy(this, enemyCoord, bombFactory))); */
     }
 
     @Override
     public void updateGame() {
+        gameTimer.updateTimer();
+        if (gameTimer.isOver()) {
+            map.triggerCollapse();
+        }
+        map.update();
         if (player.isAlive()) {
             player.update();
         }
@@ -53,11 +58,6 @@ public class BombarderoGameManager implements GameManager {
                 enemy.update();
             }
         });
-        gameTimer.updateTimer();
-        if (gameTimer.isOver()) {
-            map.triggerCollapse();
-        }
-        map.update();
     }
 
     @Override
