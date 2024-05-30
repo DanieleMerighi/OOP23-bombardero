a58a15f907d2efa31d6fd1fede9f839a8d3e0f53
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-05-30 16:42:47 +0200

    Spawned enemies

--- src/main/java/it/unibo/bombardero/core/impl/BombarderoGameManager.java
+++ src/main/java/it/unibo/bombardero/core/impl/BombarderoGameManager.java
@@ -44,7 +44,7 @@ public class BombarderoGameManager implements GameManager {
         ce = new BombarderoCollision(this);
         bombFactory = new BombFactoryImpl(this, ce);
         this.player = new Player(this, Utils.PLAYER_SPAWNPOINT, bombFactory);
-        /* Utils.ENEMIES_SPAWNPOINT.forEach(enemyCoord -> enemies.add(new Enemy(this, enemyCoord, bombFactory))); */
+        Utils.ENEMIES_SPAWNPOINT.forEach(enemyCoord -> enemies.add(new Enemy(this, enemyCoord, bombFactory)));
     }
 
     public BombarderoGameManager(final Controller controller, final boolean guideMode) {
