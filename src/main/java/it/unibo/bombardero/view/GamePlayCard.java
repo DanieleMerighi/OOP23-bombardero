bb71885bfbcd40f72bc786a465e8ef0c14d58dd6
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-06-01 15:08:35 +0200

    New class for better reuse of the Game and Guide Cards.

---
+++ src/main/java/it/unibo/bombardero/view/GamePlayCard.java
@@ -0,0 +1,12 @@
+package it.unibo.bombardero.view;
+
+import javax.swing.JPanel;
+
+/** 
+ * This class represents a JPanel containing a the view of some 
+ * playable part of the game (e.g. guide, gameplay, sandbox... etc).
+ * @author Federico Bagattoni
+ */
+public class GamePlayCard extends JPanel {
+    
+}
