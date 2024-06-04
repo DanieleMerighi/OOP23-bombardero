51f57cd6fbea9068245198fcb2fe27ae085d82ca
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-06-04 16:18:16 +0200

    Removed unused  imports and added collision with map outlines

--- src/main/java/it/unibo/bombardero/physics/impl/BombarderoCollision.java
+++ src/main/java/it/unibo/bombardero/physics/impl/BombarderoCollision.java
@@ -1,23 +1,13 @@
 package it.unibo.bombardero.physics.impl;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Map.Entry;
-import java.util.stream.Stream;
-import java.util.Set;
 import java.awt.geom.Line2D;
-import java.awt.geom.Line2D.Float;
 import java.awt.geom.Point2D;
 
-import it.unibo.bombardero.cell.BasicBomb;
-import it.unibo.bombardero.cell.Bomb;
 import it.unibo.bombardero.cell.Cell;
-import it.unibo.bombardero.cell.Flame;
-import it.unibo.bombardero.cell.Cell.CellType;
 import it.unibo.bombardero.character.Direction;
 import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.map.api.GameMap;
@@ -27,7 +17,6 @@ import it.unibo.bombardero.physics.api.CollisionEngine;
 import it.unibo.bombardero.character.Character;
 
 public class BombarderoCollision implements CollisionEngine{
-    private GameManager mgr;
     private GameMap gMap;
     private Map<Pair,Cell> map;
     private List<Character> characters;
@@ -35,11 +24,10 @@ public class BombarderoCollision implements CollisionEngine{
         Direction.UP , new Line2D.Float(new Point2D.Float(0, 0) , new Point2D.Float(13, 0)),
         Direction.DOWN , new Line2D.Float(new Point2D.Float(0, 13) , new Point2D.Float(13, 13)),
         Direction.LEFT , new Line2D.Float(new Point2D.Float(0, 0) , new Point2D.Float(0, 13)),
-        Direction.DOWN , new Line2D.Float(new Point2D.Float(13, 0) , new Point2D.Float(13, 13))
+        Direction.RIGHT , new Line2D.Float(new Point2D.Float(13, 0) , new Point2D.Float(13, 13))
     );
 
     public BombarderoCollision(GameManager mgr){
-        this.mgr=mgr;
         gMap=mgr.getGameMap();
         characters= new LinkedList<>( mgr.getEnemies()) ;
         characters.add(mgr.getPlayer());
@@ -60,7 +48,7 @@ public class BombarderoCollision implements CollisionEngine{
     public void checkCharacterCollision(Character character) {
         Optional<List<Pair>> collidingCell = getDirectionCell(character.getFacingDirection() , character.getIntCoordinate());
         if(collidingCell.isPresent()) {
-            Optional<BoundingBox> collidigBox = getDirectionCell(character.getFacingDirection() , character.getIntCoordinate())
+            Optional<BoundingBox> collidigBox = collidingCell
                 .stream().map( p -> map.get(p).getBoundingBox() )
                 .filter(b->b.isColliding(character.getBoundingBox()))
                 .findFirst();
@@ -70,11 +58,18 @@ public class BombarderoCollision implements CollisionEngine{
                         .computeCollision(collidigBox.get(), character.getFacingDirection())));
                 }
         } else {
-
+            checkCoolisionWithMapWall(character);
         }
 
     }
 
+    private void checkCoolisionWithMapWall(Character character) {
+        BoundingBox bBox = character.getBoundingBox();
+        Line2D.Float outerLine = MAP_OUTLINES.get(character.getFacingDirection());
+        if(bBox.isColliding(outerLine)) {
+            character.setCharacterPosition(bBox.computeCollision(outerLine, character.getFacingDirection()));
+        }
+    }
 
     private Optional<List<Pair>> getDirectionCell(Direction dir , Pair cDir) {
         Optional<List<Pair>> l = dir.getDiagonals(dir);
