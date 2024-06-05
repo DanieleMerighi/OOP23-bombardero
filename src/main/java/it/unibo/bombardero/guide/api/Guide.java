56cc08c48194d45b2620304d25b8befc763421bc
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-06-05 19:15:36 +0200

    Added comments, updated interface

--- src/main/java/it/unibo/bombardero/guide/api/Guide.java
+++ src/main/java/it/unibo/bombardero/guide/api/Guide.java
@@ -2,7 +2,16 @@ package it.unibo.bombardero.guide.api;
 
 import it.unibo.bombardero.character.Direction;
 import it.unibo.bombardero.core.api.GameManager;
+import it.unibo.bombardero.guide.impl.BombarderoGuideManager;
+import it.unibo.bombardero.map.api.Coord;
 
 public interface Guide extends GameManager {
     
+    /**
+     * Spawns a dummy {@link Character} in the game. The
+     * character must do nothing and serves as target for the
+     * player. 
+     */
+    void spawnDummy();
+
 }
--- src/main/java/it/unibo/bombardero/guide/impl/BombarderoGuideManager.java
+++ src/main/java/it/unibo/bombardero/guide/impl/BombarderoGuideManager.java
@@ -11,39 +11,51 @@ import it.unibo.bombardero.guide.api.Guide;
 import it.unibo.bombardero.map.api.Coord;
 import it.unibo.bombardero.map.api.Pair;
 
+/**
+ * This class represents a single instance of the game's guide
+ * it is a slightly modified {@link BombarderoGameManager} that 
+ * doesn't spawn neither enemies nor crates regularly.
+ * <p>
+ * It spawns just a crate and the player and the game proceeds at 
+ * stages, dictated by the Controller. 
+ * @author Federico Bagattoni
+ */
 public final class BombarderoGuideManager extends BombarderoGameManager implements Guide {
 
     public final static Coord PLAYER_GUIDE_SPAWNPOINT = new Coord(4.5f, 6.5f);
     public final static Pair CRATE_GUIDE_SPAWNPOINT = new Pair(8, 6);
     public final static Coord DUMMY_GUIDE_SPAWNPOINT = new Coord(7.5f, 5.5f);
 
+    /**
+     * Creates a new {@link BombarderoGuideManager}, creating a map with no
+     * crates, spawning the player in the middle of the map and a single
+     * crate to the side of the player.
+     * <p>
+     * The spawnpoints are represented by the fields {@link #PLAYER_GUIDE_SPAWNPOINT} 
+     * and {@link #CRATE_GUIDE_SPAWNPOINT}
+     * @param controller the reference to the game's {@link Controller}.
+     */
     public BombarderoGuideManager(final Controller controller) {
         super(controller, true);
+        this.getGameMap().addBreakableWall(CRATE_GUIDE_SPAWNPOINT);
     }
 
-    /* TODO: metodo update che non tiene il tempo e scandisce i vari eventi nella mappa di gioco 
-    dopo che il giocatore fa le mosse che deve fare:
-    0) mostra WASD
-    1) plauer: si muove in tutte le direzioni
-    2) mostra barra spazio
-    3) player: piazza bomba
-    4) mostra powerup e messaggio descrivendoli brevemente
-     */
-
-     @Override
+    @Override
     public void updateGame(final long elapsed) {
         this.getGameMap().update();
         this.getPlayer().update(elapsed);
     }
 
+    @Override
     public void spawnDummy() {
         addEnemy(new Dummy(DUMMY_GUIDE_SPAWNPOINT));
     }
 
     /**
      * This nested class represents a "Dummy", and NPC capable of 
-     * doing nothing. It's sole purporse is to be the target of
-     * the player during the guide.
+     * doing nothing. 
+     * <p>
+     * It's sole purporse is to be the target of the player during the guide.
      * @author Federico Bagattoni
      */
     private final class Dummy extends Character {
