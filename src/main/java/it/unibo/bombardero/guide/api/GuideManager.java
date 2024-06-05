63c26ca912bdf97caa4b4de3d0b89d406b1a11e5
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-06-05 22:50:53 +0200

    Renamed class

--- src/main/java/it/unibo/bombardero/guide/api/GuideManager.java
+++ src/main/java/it/unibo/bombardero/guide/api/GuideManager.java
@@ -5,7 +5,7 @@ import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.guide.impl.BombarderoGuideManager;
 import it.unibo.bombardero.map.api.Coord;
 
-public interface Guide extends GameManager {
+public interface GuideManager extends GameManager {
     
     /**
      * Spawns a dummy {@link Character} in the game. The
