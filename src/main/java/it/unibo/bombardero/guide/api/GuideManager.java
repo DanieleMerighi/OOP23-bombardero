a1a693454b89e8d23d4f3756022b5c1e24f5cbce
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-06-08 10:41:23 +0200

    Added JavaDoc

--- src/main/java/it/unibo/bombardero/guide/api/GuideManager.java
+++ src/main/java/it/unibo/bombardero/guide/api/GuideManager.java
@@ -5,6 +5,11 @@ import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.guide.impl.BombarderoGuideManager;
 import it.unibo.bombardero.map.api.Coord;
 
+/**
+ * This interface models a slightly different game manager, 
+ * intended to be used in the game's guide. 
+ * @author Federico Bagattoni
+*/
 public interface GuideManager extends GameManager {
     
     /**
