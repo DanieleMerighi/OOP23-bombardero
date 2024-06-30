6b179ba30f9cd6aec2f2e718b8e52b37031a788a
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-06-30 10:53:08 +0200

    Added JavaDoc comment

--- src/main/java/it/unibo/bombardero/cell/Wall.java
+++ src/main/java/it/unibo/bombardero/cell/Wall.java
@@ -1,10 +1,18 @@
 package it.unibo.bombardero.cell;
 
-import it.unibo.bombardero.cell.Cell.CellType;
 import it.unibo.bombardero.map.api.Pair;
-
+/**
+ * This class further expands the concept of a cell,
+ * creating a Wall.
+ */
 public class Wall extends AbstractCell {
 
+    /**
+     * Creates a new wall assigning it a type and a
+     * position inside the map. 
+     * @param type the type of the wall
+     * @param pos the position of the wall
+     */
     public Wall(CellType type, Pair pos) {
         super(type, pos, true);
     }
