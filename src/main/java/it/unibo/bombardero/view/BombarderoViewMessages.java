c0943e53df141c9b1d77f13b1e092b0e9f149a39
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-02 23:44:49 +0200

    Added JavaDoc

--- src/main/java/it/unibo/bombardero/view/BombarderoViewMessages.java
+++ src/main/java/it/unibo/bombardero/view/BombarderoViewMessages.java
@@ -1,11 +1,33 @@
 package it.unibo.bombardero.view;
 
+/**
+ * This enumerator represents the various types of messages that can be displayed in the view.
+ */
 public enum BombarderoViewMessages {
 
+    /**
+     * The message to explain how to move to a new player.
+     */
     EXPLAIN_MOVEMENT("To move, press the W, A, S and D"),
+
+    /**
+     * The message to explain how to place a bomb to a new user.
+     */
     PLACE_BOMB("<html>Everyone starts with one bomb, <br>press the space bar to place it near the crate and brake</html>"),
+
+    /** 
+     * The message to explain the basis of powerups to a new user. 
+     */
     EXPLAIN_POWERUP("Under the crates you can find powerups to enhance your abilities"),
+
+    /**
+     * The message to explain how bombs work to a new user.
+     */
     KILL_ENEMY("Bombs have a certain range, place a bomb near an enemy to kill him"),
+
+    /**
+     * The message to end the guide and prompt the user into choosing what to do next.
+     */
     END_GUIDE("Good! The guide has endend, you can go back to the menu or start a new game!");
 
     private String message;
@@ -14,6 +36,11 @@ public enum BombarderoViewMessages {
         this.message = message;
     }
 
+    /**
+     * Returns the string message to be displayed
+     * related to this type of message.
+     * @return the String that has to be displayed in the View
+     */
     public String getMessage() {
         return this.message;
     }
