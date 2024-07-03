a0c7aed50c8921971870b995005c918c396738e8
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-03 23:49:10 +0200

    Solved checkstyles and changed variable name

--- src/main/java/it/unibo/bombardero/view/sprites/impl/TimedBombarderoSprite.java
+++ src/main/java/it/unibo/bombardero/view/sprites/impl/TimedBombarderoSprite.java
@@ -6,51 +6,66 @@ import java.util.function.Function;
 
 import it.unibo.bombardero.view.ResourceGetter;
 
-/** 
+/**
  * This class represents a Sprite with limited lifetime choosen
- * by the user. 
+ * by the user.
  * If the assigned lifetime is less than the frames in the asset
- * the sprite will stop before returning every frame. 
+ * the sprite will stop before returning every frame.
  */
-public class TimedBombarderoSprite extends SimpleBombarderoSprite {
-    
+public final class TimedBombarderoSprite extends SimpleBombarderoSprite {
+
     private final int lifetime;
-    private final int framesPerSprite;
+    private final int ticksPerSprite;
     private long counter = 1;
 
     /**
-     * Creates a new sprite with an assigned lifetime. 
-     * @param asset
-     * @param framesPerSprite
-     * @param lifetime
+     * Creates a new sprite with an assigned lifetime and stores the passed asset
+     * to animate.
+     * 
+     * @param asset the asset to animate.
+     * @param framesPerSprite the total number of frames in the asset
+     * @param lifetime the lifetime of the sprite, in game ticks.
      */
     public TimedBombarderoSprite(final Image[] asset, final int framesPerSprite, final int lifetime) {
         super(asset, framesPerSprite);
         this.lifetime = lifetime * FOUR_FRAMES_TPS;
-        this.framesPerSprite = framesPerSprite * 7;
+        this.ticksPerSprite = framesPerSprite * FOUR_FRAMES_TPS;
     }
 
+    /**
+     * Creates a new sprite importing the resource requested before starting. The
+     * resource is also resized using the passed lambda function.
+     * @param resource the name of the resource to animate
+     * @param rg the {@link ResourceGetter} to get the resource with
+     * @param imageResizer the function that will be used to resize each image
+     * @param framesPerSprite the total number of frames in the asset
+     * @param lifetime the lifetime of the sprite, in game ticksthe total number of frames in the asset
+     */
     public TimedBombarderoSprite(
-        final String resource,
-        final ResourceGetter rg,
-        final Function<Image, Image> imageResizer,
-        final int framesPerSprite,
-        final int lifetime) {
+            final String resource,
+            final ResourceGetter rg,
+            final Function<Image, Image> imageResizer,
+            final int framesPerSprite,
+            final int lifetime) {
         super(resource, rg, imageResizer, framesPerSprite);
         this.lifetime = lifetime;
-        this.framesPerSprite = framesPerSprite;
+        this.ticksPerSprite = framesPerSprite;
     }
 
     @Override
     public void update() {
         counter++;
-        if(!isOver()) {
+        if (!isOver()) {
             super.update();
         }
     }
 
+    /**
+     * Returns wether the sprite is over or not.
+     * @return true, if the sprites is over
+     */
     public boolean isOver() {
-        return counter >= lifetime || counter >= framesPerSprite;
+        return counter >= lifetime || counter >= ticksPerSprite;
     }
-    
+
 }
