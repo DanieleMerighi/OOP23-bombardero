153e965a7d632db29d536aa01eb94eb365eb335d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-03 23:20:28 +0200

    Solved checkstyles

--- src/main/java/it/unibo/bombardero/view/sprites/api/OrientedSprite.java
+++ src/main/java/it/unibo/bombardero/view/sprites/api/OrientedSprite.java
@@ -4,9 +4,9 @@ import java.awt.Image;
 
 import it.unibo.bombardero.character.Direction;
 
-/** 
+/**
  * This interface extends the {@link Sprite} interface
- * and models an immutable sprite which handles assets moving in a 
+ * and models an immutable sprite which handles assets moving in a
  * certain direction, or standing still but facing the same direction.
  * <p>
  * Through this interface a user can know which direction the asset is
@@ -16,10 +16,11 @@ import it.unibo.bombardero.character.Direction;
  * <p>
  */
 public interface OrientedSprite extends Sprite {
-    
-    /** 
+
+    /**
      * Returns the image of the asset standing still, in the same
      * direction returned by {@link #getCurrentFacingDirection()}.
+     * 
      * @return the image of the asset standing still
      */
     Image getStandingImage();
@@ -27,13 +28,15 @@ public interface OrientedSprite extends Sprite {
     /**
      * Returns a {@link OrientedSprite} of the same resource of this object, but
      * in the direction passed as argument.
+     * 
      * @param dir the direction the new asset will face
      * @return the new sprite, facing the requested direction
      */
     OrientedSprite getNewSprite(Direction dir);
 
     /**
-     * Returns the direction that the asset is pointing towards
+     * Returns the direction that the asset is pointing towards.
+     * 
      * @return the direction the asset is pointing to
      */
     Direction getCurrentFacingDirection();
