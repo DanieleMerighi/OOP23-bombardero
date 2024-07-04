3808b13689119ccf6af8bb35d5d3a8fc47e006f2
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-07-04 18:45:29 +0200

    Added different image when button play and guide is clicked

--- src/main/java/it/unibo/bombardero/view/MenuCard.java
+++ src/main/java/it/unibo/bombardero/view/MenuCard.java
@@ -1,6 +1,5 @@
 package it.unibo.bombardero.view;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
@@ -9,8 +8,6 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -32,6 +29,8 @@ public final class MenuCard extends JPanel {
         final Image logo = graphicsEngine.getResizingEngine().getScaledMenuLogoImage(rg.loadImage("menu/logo"));
         final Image playImage = graphicsEngine.getResizingEngine().getScaledButtonImage(rg.loadImage("overlay/buttons/PLAY"));
         final Image guideImage = graphicsEngine.getResizingEngine().getScaledButtonImage(rg.loadImage("overlay/buttons/GUIDE"));
+        final Image playPressedImage = graphicsEngine.getResizingEngine().getScaledButtonImage(rg.loadImage("overlay/buttons/PLAY_PRESSED"));
+        final Image guidePressedImage = graphicsEngine.getResizingEngine().getScaledButtonImage(rg.loadImage("overlay/buttons/GUIDE_PRESSED"));
         background = graphicsEngine.getResizingEngine().getScaledBackgroundImage(rg.loadImage("menu/background"));
         // CHECKSTYLE: MagicNumber ON
 
@@ -39,7 +38,7 @@ public final class MenuCard extends JPanel {
         final GridBagConstraints gbc = new GridBagConstraints();
         play = new JButton(new ImageIcon(playImage));
         guide = new JButton(new ImageIcon(guideImage));
-        /* TODO: better style JButtons, in windows you can stil see the buttons highlight, in MacOS however no...  */
+
         guide.setBorder(null);
         play.setBorder(null);
         guide.setBorderPainted(false);
@@ -48,8 +47,9 @@ public final class MenuCard extends JPanel {
         guide.setContentAreaFilled(false);
         play.setFocusPainted(false);
         guide.setFocusPainted(false);
+        play.setPressedIcon(new ImageIcon(playPressedImage));
+        guide.setPressedIcon(new ImageIcon(guidePressedImage));
 
-        /* TODO: smaller logo and dynamic resizing menu (dependant on JFrame size) */
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridheight = 1;
@@ -87,17 +87,6 @@ public final class MenuCard extends JPanel {
             
         });
 
-        play.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                play.setBackground(Color.YELLOW); // Cambia colore quando viene premuto
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                play.setBackground(Color.BLUE); // Ripristina colore originale quando viene rilasciato
-            }
-        });
         guide.addActionListener(new ActionListener() {
 
             @Override
