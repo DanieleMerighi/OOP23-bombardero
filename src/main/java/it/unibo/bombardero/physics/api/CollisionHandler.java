35a42d6b9521cfd001d0f97c093753d24f9c8a7e
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-07-04 17:32:29 +0200

    Added javaDoc

--- src/main/java/it/unibo/bombardero/physics/api/CollisionHandler.java
+++ src/main/java/it/unibo/bombardero/physics/api/CollisionHandler.java
@@ -9,12 +9,31 @@ import it.unibo.bombardero.character.Character;
 
 public interface CollisionHandler {
 
+    /**
+     * Set the Character's position so that his BoundingBox is out from the cell's BoundingBox.
+     * @param character
+     * @param collidingCell
+     */
     void applyCharacterBoundaryCollision(Character character, Optional<Cell> collidingCell);
 
+    /**
+     * Set the Character's position so that his BoundingBox is out from line.
+     * @param character
+     * @param mapOutline
+     */
     void applyCharacterBoundaryCollision(Character character, Line2D.Float mapOutline);
 
+    /**
+     * If the Character is over a Flame he dies.
+     * @param character
+     */
     void applyFlameCollision(Character character);
 
+    /**
+     * If the Character is over a PowerUp he picks it up.
+     * @param character
+     * @param powerUp
+     */
     void applyPowerUpCollision(Character character, PowerUp powerUp);
 
 }
