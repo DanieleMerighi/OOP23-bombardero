709a8c6c5b52280df563694b064e83c6dc59824a
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-07-04 19:05:38 +0200

    Removed unused method

--- src/test/java/it/bombardero/MyGameManager.java
+++ src/test/java/it/bombardero/MyGameManager.java
@@ -1,8 +1,6 @@
 package it.bombardero;
 
-import it.unibo.bombardero.cell.Bomb;
 import it.unibo.bombardero.cell.BombFactoryImpl;
-import it.unibo.bombardero.cell.FlameImpl.FlameType;
 import it.unibo.bombardero.character.Character;
 import it.unibo.bombardero.character.Enemy;
 import it.unibo.bombardero.character.Player;
@@ -10,7 +8,6 @@ import it.unibo.bombardero.core.api.GameManager;
 import it.unibo.bombardero.map.api.GameMap;
 import it.unibo.bombardero.map.api.GenPair;
 import it.unibo.bombardero.map.impl.GameMapImpl;
-import it.unibo.bombardero.physics.impl.RectangleBoundingBox;
 
 import java.util.List;
 import java.util.Optional;
@@ -121,59 +118,6 @@ public class MyGameManager implements GameManager {
     }
 
     /**
-     * Adds a bomb to the game map.
-     *
-     * @param b the bomb
-     * @return true if the bomb was added, false otherwise
-     */
-    @Override
-    public boolean addBomb(final Bomb b, final Character character) {
-        return map.addBomb(b, b.getPos());
-    }
-
-    /**
-     * Removes a bomb from the game map.
-     *
-     * @param pos the position of the bomb
-     */
-    @Override
-    public void removeBomb(final GenPair<Integer, Integer> pos) {
-        throw new UnsupportedOperationException("Unimplemented method 'removeBomb'");
-    }
-
-    /**
-     * Adds a flame to the game map.
-     *
-     * @param type the type of flame
-     * @param pos  the position of the flame
-     */
-    @Override
-    public void addFlame(final FlameType type, final GenPair<Integer, Integer> pos) {
-        throw new UnsupportedOperationException("Unimplemented method 'addFlame'");
-    }
-
-    /**
-     * Removes a flame from the game map.
-     *
-     * @param pos the position of the flame
-     */
-    @Override
-    public void removeFlame(final GenPair<Integer, Integer> pos) {
-        throw new UnsupportedOperationException("Unimplemented method 'removeFlame'");
-    }
-
-    /**
-     * Removes a wall from the game map.
-     *
-     * @param pos the position of the wall
-     * @return true if the wall was removed, false otherwise
-     */
-    @Override
-    public boolean removeWall(final GenPair<Integer, Integer> pos) {
-        throw new UnsupportedOperationException("Unimplemented method 'removeWall'");
-    }
-
-    /**
      * Gets the time left in the game.
      *
      * @return the time left
@@ -182,29 +126,5 @@ public class MyGameManager implements GameManager {
     public Optional<Long> getTimeLeft() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getTimeLeft'");
-    }
-
-    /**
-     * Removes a power-up from the game map.
-     *
-     * @param pos the position of the power-up
-     */
-    @Override
-    public void removePowerUp(final GenPair<Integer, Integer> pos) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removePowerUp'");
-    }
-
-    /**
-     * Gets a bomb from the game map.
-     *
-     * @param pos the position of the bomb
-     * @return an optional containing the bomb, or an empty optional if no bomb is
-     *         found
-     */
-    @Override
-    public Optional<Bomb> getBomb(final GenPair<Integer, Integer> pos) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBomb'");
     }
 }
