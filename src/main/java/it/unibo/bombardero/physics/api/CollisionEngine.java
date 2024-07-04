c4e48bf983b4b788e1a2b4a948bfb9f936302ba9
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-07-04 23:47:52 +0200

    Solved checkstyle

--- src/main/java/it/unibo/bombardero/physics/api/CollisionEngine.java
+++ src/main/java/it/unibo/bombardero/physics/api/CollisionEngine.java
@@ -4,19 +4,21 @@ import it.unibo.bombardero.character.Character;
 import it.unibo.bombardero.map.api.GameMap;
 
 /**
- * this interface checks the collision of the game and solves them.
+ * This interface checks the collision of the game and solves them.
  */
 public interface CollisionEngine {
  
     /**
      * Check if character is in the same cell of flames.
      * @param character
+     * @param gMap
      */
     void checkFlameAndPowerUpCollision(Character character, GameMap gMap);
 
     /**
-     * check collision for the character with the cells in front of him.
+     * Check collision for the character with the cells in front of him.
      * @param character 
+     * @param gMap
      */
     void checkCharacterCollision(Character character, GameMap gMap);
 
