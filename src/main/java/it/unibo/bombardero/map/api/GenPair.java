f70f01748cb4daa585e15bd55aa07f496db6554e
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-07-04 16:56:14 +0200

    Changed GenPair from record to class that extends the pair from apache

--- src/main/java/it/unibo/bombardero/map/api/GenPair.java
+++ src/main/java/it/unibo/bombardero/map/api/GenPair.java
@@ -2,7 +2,21 @@ package it.unibo.bombardero.map.api;
 
 import java.util.function.Function;
 
-public record GenPair <K, V> (K x, V y) {
+import org.apache.commons.math3.util.Pair;
+
+public class GenPair <K, V> extends Pair <K, V> {
+
+    public GenPair(final K k, final V v) {
+        super(k, v);
+    }
+
+    public K x() {
+        return super.getFirst();
+    }
+
+    public V y() {
+        return super.getSecond();
+    }
 
     public GenPair <K, V> apply(final Function <GenPair<K, V>, GenPair<K, V>> f) {
         return f.apply(this);
