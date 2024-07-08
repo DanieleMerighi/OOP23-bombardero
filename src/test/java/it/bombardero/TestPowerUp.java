c56bc4f925b5607dafe062bd760d7e06b917eb6e
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2024-07-09 00:27:54 +0200

    Removed unused import

--- src/test/java/it/bombardero/TestPowerUp.java
+++ src/test/java/it/bombardero/TestPowerUp.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
