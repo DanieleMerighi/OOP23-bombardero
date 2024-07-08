c54286d4d22834d309103e8bda3fc1772b29ff76
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-07-08 22:09:30 +0200

    extracted method test direction

--- src/test/java/it/bombardero/TestCollision.java
+++ src/test/java/it/bombardero/TestCollision.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.IntegerConversion;
 
 import it.unibo.bombardero.cell.FlameImpl;
 import it.unibo.bombardero.character.Character;
@@ -17,11 +18,10 @@ import it.unibo.bombardero.physics.api.CollisionEngine;
  * This class will test bounding Box.
  */
 class TestCollision {
-    private static final GenPair<Float, Float> TR_CORNER_CELL_0_2 = new GenPair<>(0.99f, 2f);
-    private static final GenPair<Float, Float> CELL_1_0 = new GenPair<>(1.5f, 0.5f);
-    private static final GenPair<Float, Float> CELL_0_1 = new GenPair<>(0.5f, 1.5f);
-    private static final GenPair<Float, Float> TR_CORNER_CELL_2_0 = new GenPair<>(2f, 0.9f);
-    private static final GenPair<Float, Float> DR_CORNER_CELL_0_0 = new GenPair<>(0.9f, 0.9f);
+    private static final GenPair<Float, Float> L_CORNER_CELL_2_1 = new GenPair<>(2f, 1.5f);
+    private static final GenPair<Float, Float> R_CORNER_CELL_0_1 = new GenPair<>(0.9f, 1.5f);
+    private static final GenPair<Float, Float> U_CORNER_CELL_1_2 = new GenPair<>(1.5f, 2f);
+    private static final GenPair<Float, Float> D_CORNER_CELL_0_1 = new GenPair<>(1.5f, 0.9f);
     private Character character;
     private GameMap gMap;
     private MyGameManager mgr;
@@ -46,58 +46,24 @@ class TestCollision {
     @Test
     void testCharacterBoundingCollision() {
         // Direction RIGHT
-        character.setCharacterPosition(DR_CORNER_CELL_0_0);
-        gMap.addBreakableWall(new GenPair<Integer, Integer>(1, 0));
-        character.setFacingDirection(Direction.RIGHT);
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 0)).getBoundingBox().get()));
-        cEngine.checkCharacterCollision(character, gMap);
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 0)).getBoundingBox().get()));
+        testDirection(Direction.RIGHT, R_CORNER_CELL_0_1);
         // Direction DOWN
-        character.setCharacterPosition(DR_CORNER_CELL_0_0);
-        gMap.addBreakableWall(new GenPair<Integer, Integer>(0, 1));
-        character.setFacingDirection(Direction.DOWN);
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(0, 1)).getBoundingBox().get()));
-        cEngine.checkCharacterCollision(character, gMap);
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(0, 1)).getBoundingBox().get()));
+        testDirection(Direction.DOWN, D_CORNER_CELL_0_1);
         // Direction UP
-        character.setCharacterPosition(TR_CORNER_CELL_0_2);
-        gMap.addBreakableWall(new GenPair<Integer, Integer>(0, 1));
-        character.setFacingDirection(Direction.UP);
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(0, 1)).getBoundingBox().get()));
-        cEngine.checkCharacterCollision(character, gMap);
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(0, 1)).getBoundingBox().get()));
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
+        testDirection(Direction.UP, U_CORNER_CELL_1_2);
         // Direction LEFT
-        character.setCharacterPosition(TR_CORNER_CELL_2_0);
-        gMap.addBreakableWall(new GenPair<Integer, Integer>(1, 0));
-        character.setFacingDirection(Direction.LEFT);
+        testDirection(Direction.LEFT, L_CORNER_CELL_2_1);
+    }
+
+private void testDirection(final Direction dir, final GenPair<Float, Float> characterPosition) {
+        character.setCharacterPosition(characterPosition);
+        character.setFacingDirection(dir);
         assertTrue(
                 character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertTrue(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 0)).getBoundingBox().get()));
         cEngine.checkCharacterCollision(character, gMap);
         assertFalse(
                 character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 1)).getBoundingBox().get()));
-        assertFalse(
-                character.getBoundingBox().isColliding(gMap.getMap().get(new GenPair<>(1, 0)).getBoundingBox().get()));
-    }
+}
 
     /**
      * test when a character is in the same cell of a flame or a pouwerUp the
@@ -107,10 +73,10 @@ class TestCollision {
     void testFlameAndPowerUpCollision() {
         gMap.addFlame(new FlameImpl(null, new GenPair<Integer, Integer>(1, 0)), new GenPair<Integer, Integer>(1, 0));
         mgr.addPowerUp(new GenPair<Integer, Integer>(0, 1));
-        character.setCharacterPosition(CELL_0_1);
+        character.setCharacterPosition(R_CORNER_CELL_0_1);
         cEngine.checkFlameAndPowerUpCollision(character, gMap);
         assertFalse(gMap.isPowerUp(new GenPair<Integer, Integer>(0, 1)));
-        character.setCharacterPosition(CELL_1_0);
+        character.setCharacterPosition(D_CORNER_CELL_0_1);
         assertTrue(gMap.isFlame(new GenPair<Integer, Integer>(1, 0)));
         cEngine.checkFlameAndPowerUpCollision(character, gMap);
         assertFalse(character.isAlive());
