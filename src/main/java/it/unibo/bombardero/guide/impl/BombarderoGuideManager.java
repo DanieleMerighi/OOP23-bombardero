e71e850e8fdfcbc5faafeb93309c24787273b509
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-09 10:34:55 +0200

    Removed trailing spaces

--- src/main/java/it/unibo/bombardero/guide/impl/BombarderoGuideManager.java
+++ src/main/java/it/unibo/bombardero/guide/impl/BombarderoGuideManager.java
@@ -102,7 +102,7 @@ public final class BombarderoGuideManager extends BasicBombarderoGameManager imp
 
         @Override
         protected void performCharacterActions(final GameManager manager, final long elapsedTime) {
-            
+
         }
     }
 
